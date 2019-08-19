43fb0fa7d73fff4f7c6589333212e2228477afc7
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-18 20:08:17 -0700

    take first 15 using array.slice

--- components/TickerStrip.js
+++ components/TickerStrip.js
@@ -2,7 +2,7 @@ import Ticker from './Ticker'
 
 export default props => {
     console.log("The first title: ", props.movieStore.movies && props.movieStore.movies[0].title)
-    const tickers = props.movieStore.movies.map((ticker, key) => {
+    const tickers = props.movieStore.movies.slice(0, 14).map((ticker, key) => {
         const { title, price, change, percentChange, symbol } = ticker;
         return <Ticker
             key={ticker.Imbdid}
