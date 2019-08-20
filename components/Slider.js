e39f0c4077de014607696b81a896aa3f3d23df76
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-20 11:46:46 -0700

    begin build custom slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,16 +1,28 @@
+import slide1 from '../assets/images/home/slide1.jpg'
+import slide2 from '../assets/images/home/slide2.jpg'
+import slide3 from '../assets/images/home/slide3.jpg'
+import slide4 from '../assets/images/home/slide4.jpg'
+import slide5 from '../assets/images/home/slide5.jpg'
+import slide6 from '../assets/images/home/slide6.jpg'
+
 export default props => {
     return (
         <div className="slider">
             <div className="container">
-                <div className="item">1</div>
-                <div className="item">2</div>
-                <div className="item">3</div>
-                <div className="item">4</div>
-                <div className="item">5</div>
+                <img src={slide1} alt="slide1" />
+                <img src={slide2} alt="slide2" />
+                <img src={slide3} alt="slide3" />
+                <img src={slide4} alt="slide4" />
+                <img src={slide5} alt="slide5" />
+                <img src={slide6} alt="slide6" />
             </div>
             <style jsx>{`
                 .slider {
                     position: absolute;
+                }
+                img {
+                    width: 166px;
+                    margin: 0px 8px;
                 }
             `}</style>
         </div>
