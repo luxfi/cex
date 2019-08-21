1f31ccc59b955c272a35f67d5c8323c3c5067039
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-20 17:30:33 -0700

    add padding to center carousel

--- components/Slider.js
+++ components/Slider.js
@@ -12,7 +12,7 @@ const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
         breakpoint: { max: 4000, min: 3000 },
-        items: 5,
+        items: 6,
     },
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
@@ -36,33 +36,69 @@ class Slider extends React.Component {
                 <Carousel
                     responsive={responsive}
                     ssr
-                    infinite={false}
+                    infinite={true}
                     beforeChange={() => this.setState({ isMoving: true })}
                     afterChange={() => this.setState({ isMoving: false })}
-                    containerClass="first-carousel-container container"
                 >
-                    <img src={slide1} alt="slide1" />
-                    <img src={slide2} alt="slide2" />
-                    <img src={slide3} alt="slide3" />
-                    <img src={slide4} alt="slide4" />
-                    <img src={slide5} alt="slide5" />
-                    <img src={slide6} alt="slide6" />
+                    <img isMoving={this.state.isMoving} src={slide1} alt="slide1" />
+                    <img isMoving={this.state.isMoving} src={slide2} alt="slide2" />
+                    <img isMoving={this.state.isMoving} src={slide3} alt="slide3" />
+                    <img isMoving={this.state.isMoving} src={slide4} alt="slide4" />
+                    <img isMoving={this.state.isMoving} src={slide5} alt="slide5" />
+                    <img isMoving={this.state.isMoving} src={slide6} alt="slide6" />
                 </Carousel>
                 <style jsx>{`
                     .slider {
                         margin-top: 86px;
                         height 300px;
-                        width: 1440px;
+                        width: 886px;
+                        padding: 0px 278px;
+}
                     }
                     img {
-                            width: 164px;
-                        margin: 0px 8px;
+                        width: 164px;
                     }
                 `}</style>
             </div>
         )
     }
 }
+
+// class Slider extends React.Component {
+//     state = { isMoving: false };
+//     render() {
+//         return (
+//             <Carousel
+//                 responsive={responsive}
+//                 ssr
+//                 infinite={false}
+//                 beforeChange={() => this.setState({ isMoving: true })}
+//                 afterChange={() => this.setState({ isMoving: false })}
+//                 infinite
+//                 containerClass="container"
+//             >
+//                 <img isMoving={this.state.isMoving} src={slide1} alt="slide1" />
+//                 <img isMoving={this.state.isMoving} src={slide2} alt="slide2" />
+//                 <img isMoving={this.state.isMoving} src={slide3} alt="slide3" />
+//                 <img isMoving={this.state.isMoving} src={slide4} alt="slide4" />
+//                 <img isMoving={this.state.isMoving} src={slide5} alt="slide5" />
+//                 <img isMoving={this.state.isMoving} src={slide6} alt="slide6" />
+//                 <img isMoving={this.state.isMoving} src={slide1} alt="slide1" />
+//                 <style jsx>{`
+//                     .container {
+//                         margin-top: 86px;
+//                         height 300px;
+//                         width: 886px;
+//                     }
+//                     img {
+//                         width: 164px;
+//                         margin: 0px 8px;
+//                     }
+//                 `}</style>
+//             </Carousel>
+//         )
+//     }
+// }
 
 export default Slider;
 // export default () => <div className="example">Hello World!</div>
