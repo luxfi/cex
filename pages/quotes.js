6f6c503da0440678bdbb614bf49c2b3f8c1f78a0
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-31 00:55:38 -0700

    remove old code

--- pages/quotes.js
+++ pages/quotes.js
@@ -1,7 +1,22 @@
-export default function Quotes() {
-    return (
-        <div>
-            <p>This is the quotes page</p>
-        </div>
-    );
+import Link from 'next/link';
+import React from 'react'
+import Head from 'next/head'
+import { inject, observer } from 'mobx-react'
+
+@observer
+export default class Quotes extends React.Component {
+    // static async getInitialProps({ initialStoreState }) {
+    //     console.log('quotes.js getInitialProps', initialStoreState)
+    //     return {}
+    // }
+    render() {
+        return (
+            <div>
+                <p>This is the quotes page</p>
+                <Link href="/">
+                    <a>index</a>
+                </Link>
+            </div >
+        );
+    }
 }
