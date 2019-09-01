5a73167cc0304967951fd3669d095eb83b90a27c
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-31 19:49:26 -0700

    add slider

--- pages/quotes.js
+++ pages/quotes.js
@@ -1,27 +1,43 @@
 import Link from 'next/link'
 import React from 'react'
 import TickerStripLayout from "../components/generic/TickerStripLayout"
+import Slider from '../components/generic/Slider'
+import TrendingNowSliderItem from "../components/landing/TrendingNowSliderItem"
 import PageRow from '../components/generic/PageRow'
 import { inject, observer } from 'mobx-react'
 
 @inject('movieStore')
 @observer
 export default class Quotes extends React.Component {
+  state = {
+    whiteGutter: true
+  }
   static async getInitialProps({ mobxStore }) {
     await mobxStore.movieStore.fetch();
     return {
       movies: mobxStore.movieStore.movies,
+      topMovies: mobxStore.movieStore.topMovies,
     };
   }
 
+
   render() {
-    const { movies } = this.props;
+    const { topMovies, movies } = this.props
+    const trendingSliderItems = topMovies.slice(0, 14)
+      .filter(item => item.verticalImg !== "N/A")
+      .map((sliderItem, key) => {
+        const { title, Imdbid, verticalImg } = sliderItem;
+        return <TrendingNowSliderItem
+          key={Imdbid}
+          title={title}
+          imgSrc={verticalImg}
+          width="166px" />
+      })
     return (
       <TickerStripLayout movies={movies} darkNav={true}>
-        <p>This is the quotes page</p>
-        <Link href="/">
-          <a>index</a>
-        </Link>
+        <PageRow whiteGutter={this.state.whiteGutter} rowTitle={"Trending Now"} hideInnerPadding>
+          <Slider movieStore={this.props.topMovies} sliderItems={trendingSliderItems} />
+        </PageRow>
       </TickerStripLayout>
     );
   }
