31e39f9eee49d554a14743f0ea492620fb886bad
TylerJNewman <tylerjnewman18@gmail.com>
2019-08-31 18:55:25 -0700

    add ticker strip

--- components/generic/TickerStripLayout.js
+++ components/generic/TickerStripLayout.js
@@ -1,22 +1,12 @@
+import React from 'react'
 import Layout from './Layout'
 import TickerStrip from './TickerStrip'
 
-@inject('movieStore')
-@observer
-export default class TickerStripLayout extends React.Component {
-  static async getInitialProps({ mobxStore }) {
-    await mobxStore.movieStore.fetch();
-    return {
-      movies: mobxStore.movieStore.movies,
-    };
-  }
+const TickerStripLayout = ({ movies, children }) => (
+  <div>
+    <TickerStrip movies={movies} />
+    <Layout>{children}</Layout>
+  </div >
+)
 
-  render() {
-    const TickerStripLayout = ({ movies, children }) => (
-      <div>
-        <TickerStrip movies={movies} />
-        <Layout>{children}</Layout>
-      </div>
-    );
-  }
-}
+export default TickerStripLayout;
