68e73856d9d8ef586d414e68f4f90acbf7528fff
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-02 17:16:44 -0700

    add filters to slider

--- pages/quotes.js
+++ pages/quotes.js
@@ -5,44 +5,97 @@ import Slider from '../components/generic/Slider'
 import UpcomingIPOsSliderItem from "../components/landing/UpcomingIPOsSliderItem"
 import PageRow from '../components/generic/PageRow'
 import { inject, observer } from 'mobx-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faColumns, faTh, faThList } from '@fortawesome/free-solid-svg-icons'
 
 @inject('movieStore')
 @observer
 export default class Quotes extends React.Component {
-  state = {
-    whiteGutter: true
-  }
-  static async getInitialProps({ mobxStore }) {
-    await mobxStore.movieStore.fetch();
-    return {
-      movies: mobxStore.movieStore.movies,
-      topMovies: mobxStore.movieStore.topMovies,
-    };
-  }
+    state = {
+        whiteGutter: true
+    }
+    static async getInitialProps({ mobxStore }) {
+        await mobxStore.movieStore.fetch();
+        return {
+            movies: mobxStore.movieStore.movies,
+            topMovies: mobxStore.movieStore.topMovies,
+        };
+    }
 
 
-  render() {
-    const { topMovies, movies } = this.props
-    const trendingSliderItems = topMovies.slice(0, 14)
-      .filter(item => item.verticalImg !== "N/A")
-      .map((sliderItem, key) => {
-        const { title, Imdbid, verticalImg, genre, rated } = sliderItem;
-        return <UpcomingIPOsSliderItem
-          key={Imdbid}
-          title={title}
-          imgSrc={verticalImg}
-          width="166px"
-          genre={genre.split(',')[0]}
-          rated={rated}
-        />
-      })
-    return (
-      <TickerStripLayout movies={movies} darkNav={true}>
-        <PageRow whiteGutter={this.state.whiteGutter} rowTitle={"Upcoming IPOs"} hideInnerPadding>
-          <Slider movieStore={this.props.topMovies} sliderItems={trendingSliderItems} />
-        </PageRow>
-      </TickerStripLayout>
-    );
-  }
+
+
+    render() {
+        const { topMovies, movies } = this.props
+
+        const trendingSliderItems = topMovies.slice(0, 14)
+            .filter(item => item.verticalImg !== "N/A")
+            .map((sliderItem, key) => {
+                const { title, Imdbid, verticalImg, genre, rated } = sliderItem;
+                return <UpcomingIPOsSliderItem
+                    key={Imdbid}
+                    title={title}
+                    imgSrc={verticalImg}
+                    width="166px"
+                    genre={genre.split(',')[0]}
+                    rated={rated}
+                />
+            })
+
+        const filters = () => (
+            <div className="container">
+                <ul>
+                    <li>
+                        <a href="#" className="filter selected">most expected</a>
+                    </li>
+                    <li>
+                        <span className="filter">|</span>
+                    </li>
+                    <li>
+                        <a href="#" className="filter"> IPOs calender </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#" className="filter selected"><FontAwesomeIcon icon={faColumns} style={{ height: "16px" }} /></a>
+                    </li>
+                    <li>
+                        <a href="#" className="filter"><FontAwesomeIcon icon={faThList} style={{ height: "16px" }} /></a>
+                    </li>
+                    <li>
+                        <a href="#" className="filter"><FontAwesomeIcon icon={faTh} style={{ height: "16px" }} /></a>
+                    </li>
+                </ul>
+
+                <style jsx>{`
+                    .container {
+                        display: flex;
+                        flex-direction: row;
+                    }
+                    ul {
+                        list-style-type: none;
+                        display: flex;
+                        padding-left: 24px;
+                    }
+                    .filter {
+                        padding-right: 10px;
+                        color: #6a5a5a;
+                        text-decoration: none;
+                    }
+                    .selected {
+                        color: #6da7ee;
+                    }
+                `}</style>
+            </div>
+        )
+
+        return (
+            <TickerStripLayout movies={movies} darkNav={true}>
+                <PageRow whiteGutter={this.state.whiteGutter} rowTitle={"Upcoming IPOs"} hideInnerPadding filters={filters()}>
+                    <Slider movieStore={this.props.topMovies} sliderItems={trendingSliderItems} />
+                </PageRow>
+            </TickerStripLayout>
+        );
+    }
 }
 
