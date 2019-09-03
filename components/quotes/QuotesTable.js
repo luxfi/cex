6823f008a8069c1104b1b0a73dc4ec1060f31f4c
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-03 08:23:46 -0700

    add row

--- components/quotes/QuotesTable.js
+++ components/quotes/QuotesTable.js
@@ -1,28 +1,46 @@
 import TradeButton from '../generic/TradeButton.js'
-const row = (props) => {
-    <tr>
-        <th className="table-cell left" scope="row">{props.name || "Name"}</th>
-        <td className="table-cell left" >{props.symbol || "SMBL"}</td>
-        <td className="table-cell left" >{props.price || "$1.11"}</td>
-        <td className="table-cell left" >{props.change || "1.1%"}</td>
-        <td className="table-cell left" >{props.marketCap || "$111"}</td>
-        <td className="table-cell left" >{props.chart || "TBD"}</td>
-        <td className="table-cell left" ><TradeButton /></td>
-
-    </tr>
+const Row = (props) => {
+    return (
+        <tr>
+            <th className="table-cell left" scope="row">{props.name || "Name"}</th>
+            <th className="table-cell left" >{props.symbol || "SMBL"}</th>
+            <th className="table-cell left" >{props.price || "$1.11"}</th>
+            <th className="table-cell left" >{props.change || "1.1%"}</th>
+            <th className="table-cell left" >{props.marketCap || "$111"}</th>
+            <th className="table-cell left" >{props.chart || "TBD"}</th>
+            <th className="table-cell left" ><TradeButton /></th>
+            <style jsx>{`
+                .table-cell {
+                    display: table-cell;
+                    padding: 14px 40px 14px 16px;
+                    font-size: 0.875rem;
+                    text-align: left;
+                    font-weight: 400;
+                    line-height: 1.43;
+                    border-bottom: 1px solid rgba(224, 224, 224, 1);
+                    letter-spacing: 0.01071em;
+                }
+            `}</style>
+        </tr>
+    )
 }
 export default props => (
     <div className="container">
         < table className="table" >
-            <tr>
-                <th className="table-cell head left" scope="col">Name</th>
-                <th className="table-cell head left" scope="col">Symbol</th>
-                <th className="table-cell head left" scope="col">Price</th>
-                <th className="table-cell head left" scope="col">Market Cap</th>
-                <th className="table-cell head left" scope="col">Chart</th>
-                <th className="table-cell head left" scope="col">Trade</th>
-            </tr>
-            {row(props)}
+            <thead>
+                <tr>
+                    <td className="table-cell head left" scope="col">Name</td>
+                    <td className="table-cell head left" scope="col">Symbol</td>
+                    <td className="table-cell head left" scope="col">Price</td>
+                    <td className="table-cell head left" scope="col">Change</td>
+                    <td className="table-cell head left" scope="col">Market Cap</td>
+                    <td className="table-cell head left" scope="col">Chart</td>
+                    <td className="table-cell head left" scope="col">Trade</td>
+                </tr>
+            </thead>
+            <tbody>
+                <Row />
+            </tbody>
         </table>
         <style jsx>{`
             .table-cell {
