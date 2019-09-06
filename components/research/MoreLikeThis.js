85d2826d257fb9d86c883db08416c11ebb55e4d8
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-06 14:08:12 -0700

    add movie posters

--- components/research/MoreLikeThis.js
+++ components/research/MoreLikeThis.js
@@ -1,14 +1,34 @@
 export default props => {
+    const { movies } = props;
     return (
         <div className="container">
             <div className="title">
-                hello
+                More Like This
+            </div>
+            <div className="movies-container">
+                {movies
+                    .filter(movie => movie.verticalImg)
+                    .slice(0, 12)
+                    .map(movie => (
+                        <img src={movie.verticalImg} style={{ width: "72px", height: "101px", padding: "2px 4px" }} />
+                    ))}
             </div>
 
             <style jsx>{`
+                .container {
+                    display: flex;
+                    flex-direction: column;
+                }
                 .title {
                     color: #2d92dd;
-                    font-size: 35px;        
+                    font-size: 32px;
+                    margin-top: 30px;
+                    font-weight: lighter;
+
+        
+                }
+                .movies-container {
+                    margin-top: 20px;
                 }
             `}</style>
         </div>
