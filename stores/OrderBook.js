8d0260480d74b9d31893d7b885bf6696f736c29c
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-09 13:53:17 -0700

    save orderbook formatting

--- stores/OrderBook.js
+++ stores/OrderBook.js
@@ -18,20 +18,20 @@ export default class OrderBook {
   }
 
   // For DEMO
-  @action initiateDataGenerator (ticker='MDMXFR') {
+  @action initiateDataGenerator(ticker = 'MDMXFR') {
     this.ticker = ticker
     this.connected = true
 
     let order1 = new LimitOrder("order01", "bid", 13.37, 10)
     let order2 = new LimitOrder("order02", "ask", 13.38, 10)
     let order3 = new LimitOrder("order03", "bid", 13.38, 5)
-    
+
     let book = new LimitOrderBook()
-    
+
     let result = book.add(order1)
     result = book.add(order2)
     result = book.add(order3)
-    
+
     console.log(result)
 
     this.dataGenerator = setInterval(
@@ -42,7 +42,7 @@ export default class OrderBook {
     ) // Some data generator
   }
 
-  @action terminateDataGenerator () {
+  @action terminateDataGenerator() {
     clearInterval(this.dataGenerator)
     this.connected = false
   }
@@ -52,14 +52,14 @@ export default class OrderBook {
   }
 
   @computed get orders() {
-      return this.orders
+    return this.orders
   }
 
-  @computed get buyOrders () {
+  @computed get buyOrders() {
     return []
   }
 
-  @computed get sellOrders () {
+  @computed get sellOrders() {
     return []
   }
 
