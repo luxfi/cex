c904f25926255c38b51a5c62e2375112ae4fb938
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-11 23:44:51 -0700

    evenly space labels

--- components/generic/ChartIntervalControls.js
+++ components/generic/ChartIntervalControls.js
@@ -13,7 +13,7 @@ export default class ChartIntervalControls extends React.Component {
       <div className="chart-interval-controls">
         <div className="group space-single"><div className={`button ${printInterval === 1 && 'selected'}`} title="1min" onClick={e => this.handleClick(1)}>1min</div></div>
         <div className="group space-single"><div className={`button ${printInterval === 5 && 'selected'}`} title="5min" onClick={e => this.handleClick(5)}>5min</div></div>
-        <div className="group space-single"><div className={`button ${printInterval === 15 && 'selected'}`} title="15min" onClick={e => this.handleClick(15)}>15min</div></div>
+        {/* <div className="group space-single"><div className={`button ${printInterval === 15 && 'selected'}`} title="15min" onClick={e => this.handleClick(15)}>15min</div></div> */}
         <style jsx>{`
           .chart-interval-controls {
             display: flex;
--- components/utils/dateRange.js
+++ components/utils/dateRange.js
@@ -5,15 +5,47 @@ import moment from 'moment/moment.js'
 //   })
 // }, 15000)
 
-export const timelineLabels = (desiredStartTime, interval, period) => {
-  const periodsInADay = moment.duration(1, 'day').as(period);
+// export const timelineLabels = (desiredStartTime, interval, period) => {
+//   const periodsInADay = moment.duration(1, 'day').as(period);
 
+//   const timeLabels = [];
+//   const startTimeMoment = moment(desiredStartTime, 'HH:mm');
+//   for (let i = 0; i <= periodsInADay; i += interval) {
+//     startTimeMoment.add(i === 0 ? 0 : interval, period);
+//     timeLabels.push(startTimeMoment.format('HH:mm'));
+//   }
+
+//   return timeLabels;
+// };
+
+let roundNext15Min = function (m) {
+var intervals = Math.floor(m.minutes() / 15);
+if(m.minutes() % 15 != 0)
+    intervals++;
+    if(intervals == 4) {
+        m.add('hours', 1);
+        intervals = 0;
+    }
+    m.minutes(intervals * 15);
+    m.seconds(0);
+    return m;
+}
+
+export const timelineLabels = () => {
+  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+  console.log(moment().format('MMMM Do YYYY, HH:mm:ss a'))
+  let now = moment();
+  now = roundNext15Min(now)
+  // const periodsInADay = moment.duration(1, 'day').as(period);
+  let intervals = 22;
+  let interval = 15;
+  let period = "minutes"
   const timeLabels = [];
-  const startTimeMoment = moment(desiredStartTime, 'HH:mm');
-  for (let i = 0; i <= periodsInADay; i += interval) {
-    startTimeMoment.add(i === 0 ? 0 : interval, period);
+  const startTimeMoment = moment(now, 'HH:mm');
+  for (let i = 0; i <= 7; i += 1) {
+    startTimeMoment.subtract(i === 0 ? 0 : interval, period);
     timeLabels.push(startTimeMoment.format('HH:mm'));
   }
 
-  return timeLabels;
+  return timeLabels.reverse();
 };
