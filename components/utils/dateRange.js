315cbc76d612eb84fb6f9427f14ae1cc51d76205
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-12 00:31:32 -0700

    save wip

--- components/utils/dateRange.js
+++ components/utils/dateRange.js
@@ -18,14 +18,8 @@ import moment from 'moment/moment.js'
 //   return timeLabels;
 // };
 
-let roundNext15Min = function (m) {
-var intervals = Math.floor(m.minutes() / 15);
-if(m.minutes() % 15 != 0)
-    intervals++;
-    if(intervals == 4) {
-        m.add('hours', 1);
-        intervals = 0;
-    }
+let roundLast15Min = function (m) {
+  var intervals = Math.floor(m.minutes() / 15);
     m.minutes(intervals * 15);
     m.seconds(0);
     return m;
@@ -34,17 +28,24 @@ if(m.minutes() % 15 != 0)
 export const timelineLabels = () => {
   console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
   console.log(moment().format('MMMM Do YYYY, HH:mm:ss a'))
-  let now = moment();
-  now = roundNext15Min(now)
-  // const periodsInADay = moment.duration(1, 'day').as(period);
-  let intervals = 22;
-  let interval = 15;
+  let now = moment()
+  let roundedLast15Min = roundLast15Min(moment())
+  let duration = moment.duration(now.diff(roundedLast15Min))
+  let minutesDuration = duration.as('minutes')
+  let fiveMinuteIntervals = Math.floor(minutesDuration / 5)
+  // const periodsInADay = moment.duration(1, 'day').as(period)
+  let intervals = 21
+  let interval = 15
   let period = "minutes"
-  const timeLabels = [];
-  const startTimeMoment = moment(now, 'HH:mm');
-  for (let i = 0; i <= 7; i += 1) {
-    startTimeMoment.subtract(i === 0 ? 0 : interval, period);
-    timeLabels.push(startTimeMoment.format('HH:mm'));
+  const timeLabels = []
+  const startTimeMoment = moment(roundedLast15Min, 'HH:mm')
+  for (let i = 0; i <= 21; i += 1) {
+    if ( (i + fiveMinuteIntervals) % 3 === 0) {
+      startTimeMoment.subtract(i === 0 ? 0 : interval, period)
+      timeLabels.push(startTimeMoment.format('HH:mm'))
+    } else {
+      timeLabels.push('')
+    }
   }
 
   return timeLabels.reverse();
