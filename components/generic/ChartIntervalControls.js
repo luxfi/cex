ee5bcd37890678a6230c10127ba5678f7c1dd7d1
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-11 19:47:55 -0700

    create filter buttons

--- components/generic/ChartIntervalControls.js
+++ components/generic/ChartIntervalControls.js
@@ -1,50 +1,78 @@
 import React from "react";
 
 export default class ChartIntervalControls extends React.Component {
-  handleChange = (event) => {
+  handleClick = (value) => {
     const { updatePrintInterval } = this.props
-    updatePrintInterval(event.target.value)
+    updatePrintInterval(value)
   }
 
   render() {
-    const printInterval = this.props.printInterval
+    const { printInterval } = this.props
+    console.log('printInterval', printInterval)
     return (
-      <div>
-        <div className="radio">
+      <div className="chart-interval-controls">
+        {/* <div className="radio">
           <label>
-            <input type="radio" value={1} checked={printInterval === 1} name="chart-interval-controls" onChange={this.handleChange} />
+            <button value={1} checked={printInterval === 1} name="chart-interval-controls" onChange={this.handleChange} />
             1m
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" value={5} checked={printInterval === 5} name="chart-interval-controls" onChange={this.handleChange} />
+            <button value={5} checked={printInterval === 5} name="chart-interval-controls" onChange={this.handleChange} />
             5m
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" value={15} checked={printInterval === 15} name="chart-interval-controls" onChange={this.handleChange} />
+            <button type="radio" value={15} checked={printInterval === 15} name="chart-interval-controls" onChange={this.handleChange} />
             15m
             </label>
-        </div>
+        </div> */}
+        <div className="group space-single"><div className={`button ${printInterval === 1 && 'selected'}`} title="1min" onClick={e => this.handleClick(1)}>1min</div></div>
+        <div className="group space-single"><div className={`button ${printInterval === 5 && 'selected'}`} title="5min" onClick={e => this.handleClick(5)}>5min</div></div>
+        <div className="group space-single"><div className={`button ${printInterval === 15 && 'selected'}`} title="15min" onClick={e => this.handleClick(15)}>15min</div></div>
         <style jsx>{`
-          .buttons {
-            margin: 10px auto;
+          .chart-interval-controls {
             display: flex;
             flex-direction: row;
-            justify-content: space-between;
+            justify-content: flex-end;
             flex-wrap: nowrap;
-            background: #999;
           }
           .button {
-            padding: 7px 12px;
             font-size: 11px;
-            margin: .3em;
             color: white;
             text-decoration: none;
             flex-grow: 1;
             text-align: center;
+            padding: 5px !important;
+            height: 16px !important;
+            color: #4a4a4a;
+            cursor: pointer;
+            border: 1px solid;
+            border-color: #ffffff;
+            line-height: 15px;
+          }
+          .chart-interval-controls .group {
+              display: inline-block;
+              white-space: nowrap;
+              padding: 0;
+              margin: 5px 0 0;
+              height: 30px;
+              vertical-align: top;
+          }
+          .space-single {
+            padding: 0 4px 0 0;
+          }
+
+          .button:hover, .button.selected {
+            background-color: rgba(53,116,250,0.12) !important;
+            color: #8194a4 !important;
+            border-radius: 3px;
+          }
+
+          .button:hover {
+            border-color: rgb(129, 148, 164);;
           }
         `}
         </style>
