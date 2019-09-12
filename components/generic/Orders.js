790fd933cee017284cfdfe8aeae1f2dd23551100
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-12 15:25:49 -0700

    grab orders from orderbook

--- components/generic/Orders.js
+++ components/generic/Orders.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default class Orders extends React.Component {
   render() {
+    const { orders } = this.props
+    console.log('orders....', orders)
     return (
       <div className="orders">
+        {orders}
         <style jsx>{`
         `}</style>
       </div >
