ff02507f1333528679834fd12a0436a0703e2300
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-12 19:31:45 -0700

    tweak styling

--- components/generic/Orders.js
+++ components/generic/Orders.js
@@ -8,12 +8,12 @@ export default class Orders extends React.Component {
       <div className="orders">
         <div className="tables">
           <div className="orders">
-            <div className="row"><div className="cell">Price</div><div className="cell">Amt</div><div className="cell">Row</div></div>
+            <div className="row header left"><div className="cell">Price</div><div className="cell right">Amt</div><div className="cell right">Row</div></div>
             {/* {
                 orders.map((o, i) => <div key={i}>price: {o.price}, size: {o.size}, total: {(o.price * o.size).toFixed(0)}</div>)
               } */}
             {
-              orders.map((o, i) => <div key={i} className="row"><div className="cell">{o.price}</div><div className="cell">{o.size}</div><div className="cell">{(o.price * o.size).toFixed(0)}</div></div>)
+              orders.map((o, i) => <div key={i} className="row"><div className="cell left">{o.price}</div><div className="cell right">{o.size}</div><div className="cell right">{(o.price * o.size).toFixed(0)}</div></div>)
             }
           </div>
         </div>
@@ -23,8 +23,12 @@ export default class Orders extends React.Component {
             padding-top: 8px;
             display: flex;
             justify-content: space-between;
-            height: 37px;
+            font: 12px;
           } 
+          .header {
+            height: 37px;
+            padding: 9.5px 0;
+          }
           .orders {
             width: 100%;
             color: #666;
@@ -32,9 +36,13 @@ export default class Orders extends React.Component {
           .row {
             display: flex;
             justify-content: space-between;
+            align-items: center;
           }
           .cell {
-            width: 100px
+            width: 42px;
+          }
+          .right {
+            text-align: right;
           }
         `}</style>
       </div >
