0aa0541281f7491721f5a1f1cb64668d1aa3dbc1
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-24 11:27:51 -0700

    clean up sign up folder structure

--- components/forms/signup.js
+++ components/forms/signup.js
@@ -1,84 +1,80 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container';
-import Router from 'next/router'
-import * as ethers from 'ethers'
-import Api from '../../src/hanzo/api'
+import React from "react"
+import Button from "@material-ui/core/Button"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import TextField from "@material-ui/core/TextField"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
+import Link from "@material-ui/core/Link"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import { withStyles } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
+import Router from "next/router"
+import * as ethers from "ethers"
+import Api from "../../src/hanzo/api"
 
-import { watch } from 'react-referential'
-import { HANZO_KEY, HANZO_ENDPOINT } from '../../src/settings.js'
+import { watch } from "react-referential"
+import { HANZO_KEY, HANZO_ENDPOINT } from "../../src/settings.js"
 
 // import isRequired from '../../src/control-middlewares/isRequired'
-import isEmail from '../../src/control-middlewares/isEmail'
-import isPassword from '../../src/control-middlewares/isPassword'
+import isEmail from "../../src/control-middlewares/isEmail"
+import isPassword from "../../src/control-middlewares/isPassword"
 // import { renderDate } from 'react-referential-forms'
 
 const styles = theme => ({
-  '@global': {
+  "@global": {
     body: {
-      backgroundColor: theme.palette.common.white,
-    },
+      backgroundColor: theme.palette.common.white
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-});
+    margin: theme.spacing(3, 0, 2)
+  }
+})
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       emailError: false,
-      passwordError: false,
+      passwordError: false
     }
     this.submitLogin = this.submitLogin.bind(this)
   }
 
   submitLogin(e) {
     e.preventDefault()
-    if (this.state.email === "" || this.password === "")
-      return
+    if (this.state.email === "" || this.password === "") return
     // clear errors for logic flow
     this.setState({ emailError: false })
     this.setState({ passwordError: false })
-    let errors = false;
+    let errors = false
     try {
       isEmail(this.state.email)
-    }
-    catch (e) {
+    } catch (e) {
       errors = true
       this.setState({ emailError: e.message })
     }
     try {
       isPassword(this.state.password)
-    }
-    catch (e) {
+    } catch (e) {
       errors = true
       this.setState({ passwordError: e.message })
-    }
-    finally {
+    } finally {
       if (!errors) {
-        Router.push('/')
+        Router.push("/")
       }
     }
   }
@@ -90,7 +86,7 @@ class LoginForm extends React.Component {
         <CssBaseline />
         <div className={classes.paper}>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign Up
           </Typography>
           <form className={classes.form} noValidate onSubmit={this.submitLogin}>
             <TextField
@@ -104,9 +100,11 @@ class LoginForm extends React.Component {
               autoComplete="email"
               autoFocus
               error={!!this.state.emailError}
-              helperText={this.state.emailError ? this.state.emailError : ''}
+              helperText={this.state.emailError && this.state.emailError}
               value={this.state.email}
-              onChange={event => this.setState({ [event.target.name]: event.target.value })}
+              onChange={event =>
+                this.setState({ [event.target.name]: event.target.value })
+              }
             />
             <TextField
               variant="outlined"
@@ -119,12 +117,14 @@ class LoginForm extends React.Component {
               id="password"
               autoComplete="current-password"
               error={!!this.state.passwordError}
-              helperText={this.state.passwordError ? this.state.passwordError : ' '}
+              helperText={this.state.passwordError && this.state.passwordError}
               value={this.state.password}
-              onChange={event => this.setState({ [event.target.name]: event.target.value })}
+              onChange={event =>
+                this.setState({ [event.target.name]: event.target.value })
+              }
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
+              control={<Checkbox value="over18" color="primary" />}
               label="I am over 18."
             />
             <Button
@@ -134,17 +134,12 @@ class LoginForm extends React.Component {
               color="primary"
               className={classes.submit}
             >
-              Sign In
+              Sign Up
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                <Link href="/login" variant="body2">
+                  Sign in?
                 </Link>
               </Grid>
             </Grid>
