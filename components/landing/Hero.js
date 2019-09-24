266b87c45c5cbd34037e94f4c3b07152686488d7
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-24 12:58:15 -0700

    add background image

--- components/landing/Hero.js
+++ components/landing/Hero.js
@@ -1,23 +1,17 @@
 // import Nav from '../generic/Nav'
-import Header from '../layout/header'
-import HeroImg from './HeroImg'
-import HeroBanner from './HeroBanner'
+import HeroImg from "./HeroImg"
 
 export default props => {
   return (
     <div className="hero-container">
-      {/* <Nav /> */}
-      <Header />
-
       <HeroImg />
-
-      <HeroBanner />
       <style jsx>{`
-                .hero-container {
-                    position: relative;
-                    height: 679px;
-                }
-            `}</style>
+        .hero-container {
+          position: relative;
+          height: 679px;
+          overflow: hidden;
+        }
+      `}</style>
     </div>
   )
 }
