6ee46e1348401e6efdddb98f4139f963c73c0357
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-24 12:30:28 -0700

    begin homepage

--- next.config.js
+++ next.config.js
@@ -1,44 +1,48 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withStylus = require('@zeit/next-stylus')
-const rupture = require('rupture')
-const postcss      = require('poststylus')
-const autoprefixer = require('autoprefixer')
-const comments     = require('postcss-discard-comments')
+const withCSS = require("@zeit/next-css")
+const withSass = require("@zeit/next-sass")
+const withStylus = require("@zeit/next-stylus")
+const rupture = require("rupture")
+const postcss = require("poststylus")
+const autoprefixer = require("autoprefixer")
+const comments = require("postcss-discard-comments")
 
-module.exports = withCSS(withSass(withStylus({
-  stylusLoaderOptions: {
-    use: [
-      rupture(),
-      postcss([
-        autoprefixer({ browsers :'> 1%' }),
-        'rucksack-css',
-        'css-mqpacker',
-        comments({ removeAll: true })
-      ])
-    ]
-  },
-  webpack: (config, { defaultLoaders, isServer }) => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      type: 'javascript/auto',
-    })
-    
-    config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|txt|jpg|png|jpeg|svg|gif)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            context: '',
-            outputPath: 'static',
-            publicPath: '_next/static',
-            name: '[path][name].[hash].[ext]'
-          }
-        }
-      ]
-    })
+module.exports = withCSS(
+  withSass(
+    withStylus({
+      stylusLoaderOptions: {
+        use: [
+          rupture(),
+          postcss([
+            autoprefixer({ browsers: "> 1%" }),
+            "rucksack-css",
+            "css-mqpacker",
+            comments({ removeAll: true })
+          ])
+        ]
+      },
+      webpack: (config, { defaultLoaders, isServer }) => {
+        config.module.rules.push({
+          test: /\.mjs$/,
+          type: "javascript/auto"
+        })
 
-    return config
-  }
-})))
+        config.module.rules.push({
+          test: /\.(eot|woff|woff2|ttf|webp|txt|jpg|png|jpeg|svg|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                context: "",
+                outputPath: "static",
+                publicPath: "_next/static",
+                name: "[path][name].[hash].[ext]"
+              }
+            }
+          ]
+        })
+
+        return config
+      }
+    })
+  )
+)
