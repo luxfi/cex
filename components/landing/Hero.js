d1ecc40595dfccef7a6cc5c236557c6352ff495f
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-24 20:33:45 -0700

    fix minor styling

--- components/landing/Hero.js
+++ components/landing/Hero.js
@@ -23,9 +23,7 @@ export default props => {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>
-                Terminator: Dark Fate Announcement goes here
-              </h1>
+              <h1>Terminator: Dark Fate Announcement goes here</h1>
               <h4>
                 It will feel no pity. No remorse. No pain. No fear. It will have
                 only one purpose: to return to the present and prevent the
