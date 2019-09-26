697d85312abf818472c828992764d26e1983ec65
Timothy Messer <tmesser@verus.io>
2019-09-26 11:09:53 -0500

    Slight cleanup and break out

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,8 +5,6 @@ import { makeStyles, withStyles } from "@material-ui/core/styles"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
 import AppBar from "@material-ui/core/AppBar"
-import Typography from "@material-ui/core/Typography"
-import Box from "@material-ui/core/Box"
 
 import { inject, observer } from "mobx-react"
 
@@ -18,95 +16,58 @@ import styles from "../assets/jss/views/landingPage.js"
 // Sections for this page
 import InvestorTopPicksSection from "../views/LandingPage/Sections/InvestorTopPicksSection"
 import PortfolioCta from "../views/ProfilePage/PortfolioCta"
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
+import TabPanel from "../views/ProfilePage/TabPanel"
 
 @inject("store")
 @observer
 class Portfolio extends React.Component {
 
-  TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <Typography
-        component="div"
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        <Box p={3}>{children}</Box>
-      </Typography>
-    );
-  }
-  
-  a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
   render() {
-    TabPanel.propTypes = {
-      children: PropTypes.node,
-      index: PropTypes.any.isRequired,
-      value: PropTypes.any.isRequired,
-    };
-    
-    const [value, setValue] = React.useState(0);
-  
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
     // const { movieStore } = this.props.store
+    // TODO: The Tabs and TabPanel tags usually have value={value} functional notes, 
+    // but I have removed those to get structure working.
+    // Tabs also has an onChange propery.
+    /*
+     <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    */
     const { classes } = this.props
     return (
       <>
         <div style={{ height: "140px" }}></div>
         <div className={`${classes.main} ${classes.mainRaised}`}>
           <AppBar position="static">
-            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-              <Tab label="Portfolio" {...a11yProps(0)} />
-              <Tab label="Trade" {...a11yProps(1)} />
-              <Tab label="Benefits" {...a11yProps(2)} />
-              <Tab label="Newsfeed" {...a11yProps(3)} />
+            <Tabs aria-label="simple tabs example">
+              <Tab label="Portfolio" />
+              <Tab label="Trade" />
+              <Tab label="Benefits" />
+              <Tab label="Newsfeed" />
             </Tabs>
           </AppBar>
-          <TabPanel value={value} index={0}>
+          <TabPanel index={0}>
             Portfolio
           </TabPanel>
-          <TabPanel value={value} index={1}>
+          <TabPanel index={1}>
             Trade
           </TabPanel>
-          <TabPanel value={value} index={2}>
+          <TabPanel index={2}>
             Benefits
           </TabPanel>
-          <TabPanel value={value} index={3}>
+          <TabPanel index={3}>
             Newsfeed
           </TabPanel>
           <div className={classes.container}>
---
+++ views/ProfilePage/TabPanel.js
@@ -0,0 +1,21 @@
+
+
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+
+export default props => {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
