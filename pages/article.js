8b02ad8616695548ac711c699f923bb1cf032e94
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-26 20:17:04 -0700

    try new design

--- pages/article.js
+++ pages/article.js
@@ -91,7 +91,7 @@ class Index extends React.Component {
     const { classes } = this.props
     return (
       <>
-        <div style={{ height: "140px" }}></div>
+        <div style={{ height: "112px" }}></div>
         <div className={`${classes.main} ${classes.mainRaised}`}>
           <div className={classes.container}>
             <Breadcrumbs />
