a5278a31031efb3a8cdaf2bb935321d2a89268e8
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-29 15:19:57 -0700

    fix protrader cta style to flexbox

--- views/ProfilePage/ProTraderCTA.js
+++ views/ProfilePage/ProTraderCTA.js
@@ -4,11 +4,10 @@ import classNames from "classnames"
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles"
 
-// @material-ui/icons
-
 // core components
 import Button from "../../components/CustomButtons/Button.js"
 import ContentLoader from "react-content-loader"
+import Modal from "../../components/Modal.js"
 
 // import styles from "../../../assets/jss/views/landingPageSections/investorTopPicksStyle.js"
 import styles from "../../assets/jss/views/portfolioPageSections/proTraderCTAStyle.js"
@@ -28,18 +27,107 @@ const MyLoader = () => (
   </ContentLoader>
 )
 
+const RemindLater = ({ classes }) => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <>
+      <Button
+        onClick={handleOpen}
+        style={{
+          marginLeft: "20px"
+        }}
+      >
+        Remind Later
+      </Button>
+      <Modal handleClose={handleClose} open={open} title="What is ESX?">
+        <p>ESX is a film investing platform for everyone.</p>{" "}
+        <p>
+          We allow regular people — not just wealthy film producers — to invest
+          in promising films, with as little as $10 or as much as $100,000 per
+          investment.
+        </p>{" "}
+        <p>
+          ESX was created to democratize fundraising for film while giving
+          anyone the chance to back the next greatest film.
+        </p>
+      </Modal>
+    </>
+  )
+}
+
+const JoinNow = ({ classes }) => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <>
+      <Button
+        color="outlined"
+        style={{
+          color: "black",
+          marginLeft: "20px"
+        }}
+        onClick={handleOpen}
+      >
+        Join Now
+      </Button>
+      <Modal handleClose={handleClose} open={open} title="What is ESX?">
+        <p>ESX is a film investing platform for everyone.</p>{" "}
+        <p>
+          We allow regular people — not just wealthy film producers — to invest
+          in promising films, with as little as $10 or as much as $100,000 per
+          investment.
+        </p>{" "}
+        <p>
+          ESX was created to democratize fundraising for film while giving
+          anyone the chance to back the next greatest film.
+        </p>
+      </Modal>
+    </>
+  )
+}
+
 export default props => {
   const classes = useStyles()
 
   return (
     <>
-      <div className={classes.section}>
-        <h2 className={classes.title}>Become a Pro Trader.</h2>
-        <span>
-          Pro Traders make a lot more money and know a lot more things than non
-          pro traders.
-        </span>
-        <Button
+      <div
+        className={classes.section}
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "60%",
+            flexDirection: "column"
+          }}
+        >
+          <h2 className={classes.title}>Become a Pro Trader.</h2>
+          <span>
+            Pro Traders make a lot more money and know a lot more things than
+            non pro traders.
+          </span>
+        </div>
+        <div style={{ display: "flex", width: "40%" }}>
+          <RemindLater />
+          <JoinNow />
+        </div>
+        {/* <Button
           style={{
             marginLeft: "20px"
           }}
@@ -57,7 +145,7 @@ export default props => {
           }}
         >
           Join Now
-        </Button>
+        </Button> */}
       </div>
       <style jsx>{`
         .hero-container {
