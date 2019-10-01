f7a115afaef5c0d511808800a661686663718c8b
TylerJNewman <tylerjnewman18@gmail.com>
2019-09-30 23:41:49 -0700

    snackbar error working for login

--- stores/UIStore.js
+++ stores/UIStore.js
@@ -2,12 +2,17 @@ import { action, observable, computed } from "mobx"
 
 export default class UIStore {
   @observable snackBarOpen = false
+  @observable error = ""
 
   constructor(initialData, hanzoApi) {
     this.api = hanzoApi
   }
 
   @computed get open() {
+    return this.snackBarOpen
+  }
+
+  @computed get error() {
     return this.snackBarOpen
   }
 
