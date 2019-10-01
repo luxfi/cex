88f3d59b6c03ccf7ecaa8f566c5c03feeede762d
Everett <newetown@hotmail.com>
2019-10-01 14:58:38 -0700

    WIP with next.config

--- assets/tempData/movies.js
+++ assets/tempData/movies.js
@@ -1,4 +1,4 @@
-export default [
+module.exports = [
   {
     name: "Terminator: Dark Fate",
     movieSlug: "terminator-dark-fate",
--- next.config.js
+++ next.config.js
@@ -41,6 +41,46 @@ module.exports = withCSS(
         })
 
         return config
+      },
+      async exportPathMap () {
+        const articlesFromJson = require('./assets/tempData/articles')
+        const moviesFromJson = require('./assets/tempData/movies')
+
+        const articles = articlesFromJson.reduce(
+          (articles, article) => {
+            return Object.assign({}, articles, {
+              [`/article/${article.articleSlug}`]: {
+                page: '/article',
+                query: { slug: article.articleSlug }
+              }
+            })
+          },
+          {}
+        )
+
+        console.log('Built articles', articles)
+        
+        const movies = moviesFromJson.reduce(
+          (movies, movie) =>
+            Object.assign({}, movies, {
+              [`/film/${movie.movieSlug}`]: {
+                page: '/film',
+                query: { slug: movie.movieSlug }
+              }
+            }),
+          {}
+        )
+
+        console.log('Built movies', movies)
+    
+        // combine the map of post pages with the home
+        return Object.assign({}, articles, movies, {
+          '/': { page: '/' },
+          '/login': { page: '/login' },
+          '/signup': { page: '/signup' },
+          '/portfolio': { page: '/portfolio' },
+          '/account/kyc': { page: '/account/kyc' },
+        })
       }
     })
   )
