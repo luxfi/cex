93217e6f73c7d97f47a4a29c641149f6d0bc6b9e
TylerJNewman <tylerjnewman18@gmail.com>
2019-10-01 18:29:54 -0700

    add candlestick

--- components/generic/FakeCandlestickChart.js
+++ components/generic/FakeCandlestickChart.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import { XAxis, YAxis, FlexibleWidthXYPlot, ChartLabel } from 'react-vis'
-import Candlestick from './Candlestick'
-import { observer } from 'mobx-react'
+import React from "react"
+import { XAxis, YAxis, FlexibleWidthXYPlot, ChartLabel } from "react-vis"
+import Candlestick from "./Candlestick"
+import { observer } from "mobx-react"
 
 @observer
 export default class CandlestickExample extends React.Component {
   render() {
     const { data, yDomain, labels, marginLeft } = this.props
     let domain = yDomain || [13, 13.7]
-    let xPercentFirst = -0.0050
+    let xPercentFirst = -0.005
     let xPercentLast = 1 - 0.01
-    let xPercent = -0.0050
-    let range = (xPercentLast - xPercentFirst)
+    let xPercent = -0.005
+    let range = xPercentLast - xPercentFirst
     let intervals = 21.0
     let increments = range / intervals
     let array = []
-    array.push(xPercent);
+    array.push(xPercent)
     while (xPercent < xPercentLast) {
-      xPercent += increments;
+      xPercent += increments
       array.push(xPercent)
     }
+    //     .rv-xy-plot__series--label {
+    //   fill: white;
+    // }
     return (
       <div className="candlestick-example">
         <div className="chart">
-          <FlexibleWidthXYPlot animation yDomain={domain} height={450} style={{ marginLeft: "-12px" }}>
+          <FlexibleWidthXYPlot
+            animation
+            yDomain={domain}
+            height={450}
+            style={{ marginLeft: "-12px" }}
+          >
             {/* <XAxis /> */}
-            <YAxis />
+            <YAxis color="white" />
             <Candlestick
               colorType="literal"
               opacityType="literal"
@@ -41,8 +49,9 @@ export default class CandlestickExample extends React.Component {
                   includeMargin={false}
                   xPercent={array[i]}
                   yPercent={1.089}
+                  color="white"
                 />
-              );
+              )
             })}
           </FlexibleWidthXYPlot>
         </div>
@@ -57,9 +66,15 @@ export default class CandlestickExample extends React.Component {
           .dashed-example-line {
             stroke-dasharray: 2, 2;
           }
+          .rv-xy-plot__axis.rv-xy-plot__axis--vertical {
+            fill: white;
+          }
+          text {
+            fill: white;
+          }
         }
         `}</style>
-      </div >
-    );
+      </div>
+    )
   }
 }
