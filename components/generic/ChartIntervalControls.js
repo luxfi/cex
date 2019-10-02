d6376569901e960a32798b3d1d7d66332614c9cf
TylerJNewman <tylerjnewman18@gmail.com>
2019-10-01 18:56:45 -0700

    add material toggle button from 1min to 5min

--- components/generic/ChartIntervalControls.js
+++ components/generic/ChartIntervalControls.js
@@ -1,19 +1,78 @@
 import React from "react"
+import FormatAlignLeftIcon from "@material-ui/icons/FormatAlignLeft"
+import FormatAlignCenterIcon from "@material-ui/icons/FormatAlignCenter"
+import FormatAlignRightIcon from "@material-ui/icons/FormatAlignRight"
+import FormatAlignJustifyIcon from "@material-ui/icons/FormatAlignJustify"
+import FormatBoldIcon from "@material-ui/icons/FormatBold"
+import FormatItalicIcon from "@material-ui/icons/FormatItalic"
+import FormatUnderlinedIcon from "@material-ui/icons/FormatUnderlined"
+import FormatColorFillIcon from "@material-ui/icons/FormatColorFill"
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
+import Typography from "@material-ui/core/Typography"
+import ToggleButton from "@material-ui/lab/ToggleButton"
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup"
 import Button from "@material-ui/core/Button"
+import { makeStyles } from "@material-ui/core/styles"
 
-export default class ChartIntervalControls extends React.Component {
-  handleClick = value => {
-    const { updatePrintInterval } = this.props
-    updatePrintInterval(value)
+const useStyles = makeStyles(theme => ({
+  toggleContainer: {
+    margin: theme.spacing(2, 0)
+  }
+}))
+
+const ToggleButtons = ({ updatePrintInterval }) => {
+  const [alignment, setAlignment] = React.useState("5min")
+  // const [formats, setFormats] = React.useState(() => ["5min"])
+
+  // const handleFormat = (event, newFormats) => {
+  //   setFormats(newFormats)
+  // }
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment)
   }
 
+  const classes = useStyles()
+
+  return (
+    <div className={classes.toggleContainer}>
+      <ToggleButtonGroup
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        <ToggleButton
+          value="1min"
+          aria-label="left aligned"
+          onClick={e => updatePrintInterval(1)}
+        >
+          1min
+        </ToggleButton>
+        <ToggleButton
+          value="5min"
+          aria-label="centered"
+          onClick={e => updatePrintInterval(5)}
+        >
+          5min
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  )
+}
+
+export default class ChartIntervalControls extends React.Component {
   render() {
     const { printInterval } = this.props
+
+    const { updatePrintInterval } = this.props
+
     // console.log('printInterval', printInterval)
     return (
       <div className="chart-interval-controls">
         <div className="group space-single">
-          <Button
+          <ToggleButtons updatePrintInterval={updatePrintInterval} />
+          {/* <Button
             className={`button ${printInterval === 1 && "selected"}`}
             title="1min"
             onClick={e => this.handleClick(1)}
@@ -30,7 +89,7 @@ export default class ChartIntervalControls extends React.Component {
             color="white"
           >
             5min
-          </Button>
+          </Button> */}
         </div>
         {/* <div className="group space-single"><div className={`button ${printInterval === 15 && 'selected'}`} title="15min" onClick={e => this.handleClick(15)}>15min</div></div> */}
         <style jsx>
