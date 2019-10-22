eae17a806f50c25b7611f12687d2ed2070954f3f
artyom <artemis.primedev@gmail.com>
2019-10-22 12:51:21 -0700

    contact form using Formik / Yup

--- components/contact/contactForm.style.js
+++ components/contact/contactForm.style.js
@@ -1,5 +1,22 @@
-export default {
+export default (theme) => ({
 
+  contactOuter: {
+    width: "600px"
+  },
 
-  
-}
+  fieldsOuter: {
+    display: "flex",
+    flexDirection: "column"
+  },
+
+  textField: {
+    maxWidth: "200px"
+  },
+
+  buttonsOuter: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: theme.spacing(2)
+  },
+})
