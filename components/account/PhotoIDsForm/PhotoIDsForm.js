4a960c4397c859d57df16b702724606212c33181
TylerJNewman <tylerjnewman18@gmail.com>
2019-10-24 15:13:19 -0700

    add upload and photo icons

--- components/account/PhotoIDsForm/PhotoIDsForm.js
+++ components/account/PhotoIDsForm/PhotoIDsForm.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import WebCamModal from "./"
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Backup'
+import CameraIcon from '@material-ui/icons/CameraAlt'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const products = [
   { name: 'Face', desc: 'Required', price: 'photo here' },
   { name: 'ID Front', desc: 'Required', price: 'photo here' },
   { name: 'ID Back', desc: 'Required', price: 'photo here' },
-];
+]
 const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
   listItem: {
     padding: theme.spacing(1, 0),
   },
@@ -20,10 +28,15 @@ const useStyles = makeStyles(theme => ({
   title: {
     marginTop: theme.spacing(2),
   },
-}));
+}))
 
 export default function PhotoIDs() {
-  const classes = useStyles();
+  const classes = useStyles()
+
+  const onTakePhoto = (dataUri) => {
+    // Do stuff with the dataUri photo...
+    console.log('takePhoto', dataUri)
+  }
 
   return (
     <>
@@ -34,10 +47,20 @@ export default function PhotoIDs() {
         {products.map(product => (
           <ListItem className={classes.listItem} key={product.name}>
             <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
+            <Fab aria-label="add" className={classes.fab}>
+              <AddIcon />
+            </Fab>
+            <Fab aria-label="camera" className={classes.fab}>
+              <CameraIcon />
+            </Fab>
+            <Fab disabled aria-label="delete" className={classes.fab}>
+              <DeleteIcon />
+            </Fab>
+            {/* <Typography variant="body2">{product.price}</Typography> */}
           </ListItem>
         ))}
       </List>
     </>
-  );
+  )
 }
+
