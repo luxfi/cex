807f97275422a02293ad90559398d1bdf822580c
TylerJNewman <tylerjnewman18@gmail.com>
2019-10-25 01:45:10 -0700

    shift styles up in the code so they are read correctly

--- components/account/PhotoIDsForm/PhotoIDsForm.js
+++ components/account/PhotoIDsForm/PhotoIDsForm.js
@@ -1,72 +1,117 @@
-import React, { useCallback, createRef } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import Box from '@material-ui/core/Box'
-import Paper from '@material-ui/core/Paper'
-import Fab from '@material-ui/core/Fab'
-import AddIcon from '@material-ui/icons/Backup'
-import CameraIcon from '@material-ui/icons/CameraAlt'
-import DeleteIcon from '@material-ui/icons/Delete'
+import React, { useCallback, createRef } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import Box from "@material-ui/core/Box"
+import Paper from "@material-ui/core/Paper"
+import Fab from "@material-ui/core/Fab"
+import AddIcon from "@material-ui/icons/Backup"
+import CameraIcon from "@material-ui/icons/CameraAlt"
+import DeleteIcon from "@material-ui/icons/Delete"
 import { CustomModal } from "../../app"
-import 'react-html5-camera-photo/build/css/index.css'
-import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo'
-import { useDropzone } from 'react-dropzone'
+import "react-html5-camera-photo/build/css/index.css"
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo"
+import { useDropzone } from "react-dropzone"
 
 const useStyles = makeStyles(theme => ({
   fab: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   listItem: {
-    padding: theme.spacing(1, 0),
+    padding: theme.spacing(1, 0)
   },
   total: {
-    fontWeight: '700',
+    fontWeight: "700"
   },
   title: {
-    marginTop: theme.spacing(2),
-  },
+    marginTop: theme.spacing(2)
+  }
 }))
 
-export default function PhotoIDs({ documents0, documents1, documents2, setValue }) {
+export default function PhotoIDs({
+  documents0,
+  documents1,
+  documents2,
+  setValue
+}) {
   const classes = useStyles()
 
   const [openCam, setOpenCam] = React.useState(false)
   const [currentDocument, setCurrentDocument] = React.useState(false)
   const [files, setFiles] = React.useState([])
 
+  
+  
+  
+  
+  
   const { getRootProps, getInputProps, open } = useDropzone({
-    accept: 'image/*',
+    accept: "image/*",
     onDrop: acceptedFiles => {
-      setFiles(acceptedFiles.map(file => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      })));
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file)
+          })
+        )
+      )
     }
   })
+
+    const thumbsContainer = {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      marginTop: 16
+    }
+
+    const thumb = {
+      display: "inline-flex",
+      borderRadius: 2,
+      border: "1px solid #eaeaea",
+      marginBottom: 8,
+      marginRight: 8,
+      width: 100,
+      height: 100,
+      padding: 4,
+      boxSizing: "border-box"
+    }
+
+    const thumbInner = {
+      display: "flex",
+      minWidth: 0,
+      overflow: "hidden"
+    }
+
+    const img = {
+      display: "block",
+      width: "auto",
+      height: "100%"
+    }
 
   const thumbs = files.map(file => (
     <div style={thumb} key={file.name}>
       <div style={thumbInner}>
-        <img
-          src={file.preview}
-          style={img}
-        />
+        <img src={file.preview} style={img} />
       </div>
     </div>
-  ));
+  ))
 
   const onDrop = useCallback(acceptedFiles => {
     console.log(acceptedFiles)
   }, [])
 
-  React.useEffect(() => () => {
-    // Make sure to revoke the data uris to avoid memory leaks
-    files.forEach(file => URL.revokeObjectURL(file.preview));
-  }, [files]);
+  React.useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach(file => URL.revokeObjectURL(file.preview))
+    },
+    [files]
+  )
 
-  const handleOpenCam = (currentDoc) => {
+  const handleOpenCam = currentDoc => {
     setCurrentDocument(currentDoc)
     setOpenCam(true)
   }
@@ -75,58 +120,45 @@ export default function PhotoIDs({ documents0, documents1, documents2, setValue 
     setOpenCam(false)
   }
 
-  const onTakePhoto = (dataUri) => {
+  const onTakePhoto = dataUri => {
     setValue(currentDocument, dataUri)
   }
 
-  const thumbsContainer = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 16
-  };
-
-  const thumb = {
-    display: 'inline-flex',
-    borderRadius: 2,
-    border: '1px solid #eaeaea',
-    marginBottom: 8,
-    marginRight: 8,
-    width: 100,
-    height: 100,
-    padding: 4,
-    boxSizing: 'border-box'
-  };
-
-  const thumbInner = {
-    display: 'flex',
-    minWidth: 0,
-    overflow: 'hidden'
-  };
-
-  const img = {
-    display: 'block',
-    width: 'auto',
-    height: '100%'
-  };
-
-
   const photos = [
-    { name: 'Face', desc: 'Required', price: 'photo here', dataUri: documents0, currentDoc: "documents0" },
-    { name: 'ID Front', desc: 'Required', price: 'photo here', dataUri: documents1, currentDoc: "documents1" },
-    { name: 'ID Back', desc: 'Required', price: 'photo here', dataUri: documents2, currentDoc: "documents2" },
+    {
+      name: "Face",
+      desc: "Required",
+      price: "photo here",
+      dataUri: documents0,
+      currentDoc: "documents0"
+    },
+    {
+      name: "ID Front",
+      desc: "Required",
+      price: "photo here",
+      dataUri: documents1,
+      currentDoc: "documents1"
+    },
+    {
+      name: "ID Back",
+      desc: "Required",
+      price: "photo here",
+      dataUri: documents2,
+      currentDoc: "documents2"
+    }
   ]
   return (
     <>
-      <CustomModal
-        handleClose={handleCloseCam}
-        open={openCam}
-      >
+      <CustomModal handleClose={handleCloseCam} open={openCam}>
         <Camera
           idealFacingMode={FACING_MODES.USER}
-          onTakePhoto={(dataUri) => { onTakePhoto(dataUri) }}
+          onTakePhoto={dataUri => {
+            onTakePhoto(dataUri)
+          }}
           onTakePhotoAnimationDone={handleCloseCam}
-          onCameraError={(error) => { console.log(error) }}
+          onCameraError={error => {
+            console.log(error)
+          }}
           idealResolution={{ width: 640, height: 480 }}
           imageType={IMAGE_TYPES.JPG}
           imageCompression={0.97}
@@ -134,9 +166,7 @@ export default function PhotoIDs({ documents0, documents1, documents2, setValue 
           isImageMirror={false}
         />
       </CustomModal>
-      <aside style={thumbsContainer}>
-        {thumbs}
-      </aside>
+      <aside style={thumbsContainer}>{thumbs}</aside>
       <Typography variant="h6" gutterBottom>
         PhotoIDs
       </Typography>
@@ -144,12 +174,12 @@ export default function PhotoIDs({ documents0, documents1, documents2, setValue 
         {photos.map(photo => (
           <ListItem className={classes.listItem} key={photo.name}>
             <ListItemText primary={photo.name} secondary={photo.desc} />
-            {!photo.dataUri ?
+            {!photo.dataUri ? (
               <>
                 <Box mr={2}>
-                  <Paper {...getRootProps({ className: 'dropzone' })}>
-                      <input {...getInputProps()} />
-                      <p style={{ padding: "32px" }}>Drag 'n' drop image here</p>
+                  <Paper {...getRootProps({ className: "dropzone" })}>
+                    <input {...getInputProps()} />
+                    <p style={{ padding: "32px" }}>Drag 'n' drop image here</p>
                   </Paper>
                 </Box>
                 <Fab aria-label="add" className={classes.fab}>
@@ -159,27 +189,25 @@ export default function PhotoIDs({ documents0, documents1, documents2, setValue 
                   <CameraIcon onClick={() => handleOpenCam(photo.currentDoc)} />
                 </Fab>
               </>
-                :
+            ) : (
               <img
-                  src={photo.dataUri}
-                  style={{
-                    maxWidth: "300px",
-                    padding: "24px"
-                  }}
-                />
-                }
+                src={photo.dataUri}
+                style={{
+                  maxWidth: "300px"
+                }}
+              />
+            )}
             <Fab
-                  disabled={photo.dataUri ? false : true}
-                  aria-label="delete"
-                  className={classes.fab}
-                  onClick={() => setValue(photo.currentDoc, "")}
-                >
-                  <DeleteIcon />
-                </Fab>
-              </ListItem>
+              disabled={photo.dataUri ? false : true}
+              aria-label="delete"
+              className={classes.fab}
+              onClick={() => setValue(photo.currentDoc, "")}
+            >
+              <DeleteIcon />
+            </Fab>
+          </ListItem>
         ))}
       </List>
     </>
-      )
-    }
-    
+  )
+}
