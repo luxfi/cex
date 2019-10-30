162fbc2f40416b6d27cdb6082d3d7bf308ccf834
artyom <artemis.primedev@gmail.com>
2019-10-29 19:52:10 -0700

    working aux pages

--- components/app/Footer/Footer.js
+++ components/app/Footer/Footer.js
@@ -113,9 +113,11 @@ const footers = [
       },
       {
         title: 'Project FAQ',
+        link: 'projectFaq'
       },
       {
         title: 'Risks',
+        link: 'risks'
       },
       {
         title: 'Contact Us',
