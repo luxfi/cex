048543cad28a3247129d3f58f17ff4a0d00c9cef
David Tai <david@verus.io>
2019-11-01 12:36:48 -0500

    merged the pages together

--- components/portfolio/TradeView/TradeView.js
+++ components/portfolio/TradeView/TradeView.js
@@ -2,17 +2,51 @@
 import React, { useState } from "react"
 
 import { ActivePositions, TopInvestmentPicks} from "../../app"
+import PortfolioPagePortfolioMeta from '../PortfolioMeta/PortfolioMeta'
+import WatchlistSection from '../WatchlistSection/WatchlistSection'
 
 const TradeView = props => {
-  const { tabIdx, index, investments, findMovieByTicker, store } = props
+  const {
+    tabIdx,
+    index,
+    investments,
+    findMovieByTicker,
+    store,
+    findMovie,
+    holdings,
+    weeklyChange,
+    rank,
+    rankPercent,
+    benefits,
+    benefitsMonthly,
+    topCategories,
+    watchlist,
+    removeFromWatchlist,
+  } = props
 
   // Hide the tab
   if (tabIdx !== index) return null
 
   return (
     <div>
-      <TopInvestmentPicks store={store} />
+      <PortfolioPagePortfolioMeta
+        holdings={holdings}
+        weeklyChange={weeklyChange}
+        rank={rank}
+        rankPercent={rankPercent}
+        benefits={benefits}
+        benefitsMonthly={benefitsMonthly}
+        topCategories={topCategories}
+      />
       <ActivePositions investments={investments} findMovieByTicker={findMovieByTicker} />
+      <TopInvestmentPicks store={store} />
+      <WatchlistSection
+        watchlist={watchlist}
+        findMovie={findMovie}
+        remove={removeFromWatchlist}
+      />
+      <br />
+      <br />
     </div>
   )
 }
