d9ae8ba6b586e9508b62cc7449f9f150e230d289
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-06 13:15:33 -0800

    ready for merge

--- components/app/Chart/Chart.js
+++ components/app/Chart/Chart.js
@@ -6,10 +6,11 @@ import {
   ToggleVisibleChart
 } from "../"
 import { timelineLabels } from "../../utils/dateRange"
+import { Element } from "react-scroll"
 import dynamic from "next/dynamic"
 import { Toolbar, Grid } from "@material-ui/core"
 import { useState } from "react"
-import { StockRechart } from "../"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 
 const TVChartContainer = dynamic(
   async () => {
@@ -22,11 +23,33 @@ const TVChartContainer = dynamic(
   }
 )
 
+function getActiveChart(activeChart, { chartData, yDomain, labels }) {
+  switch (activeChart) {
+    case "candlestick":
+      return (
+        <ChartCandlestickFake
+          data={chartData}
+          yDomain={yDomain}
+          labels={labels}
+        />
+      )
+    case "line-chart":
+      return (
+        <ChartLineSeries data={chartData} yDomain={yDomain} labels={labels} />
+      )
+    case 2:
+      return null
+    default:
+      return null
+  }
+}
+
 export default props => {
   const {
     chartData,
     yDomain,
     updatePrintInterval,
+    printInterval,
     activeChart,
     buyOrders,
     sellOrders,
@@ -41,33 +64,44 @@ export default props => {
   let labels = timelineLabels()
   const [visible, setVisible] = useState(false)
   return (
-    <div className="container">
+    <Element className="container">
+      <Toolbar>
+          <ChartIntervalControls
+            updatePrintInterval={updatePrintInterval}
+            activeChart={activeChart}
+          />
+        <div style={{ flexGrow: 1 }} />
+        <ToggleVisibleChart setActiveChart={setActiveChart} />
+      </Toolbar>
       {/* <TVChartContainer /> */}
-      <StockRechart/>
-        <div className="container-row space-between">
-          <BuySellForm
-            buttonColor="green"
-            buttonText="BUY"
-            orderType="bid"
-            ticker={ticker}
-            orders={buyOrders}
-            orderBook={orderBook}
-            onExecute={onExecute}
-            movieCategories={movieCategories}
-          />
-          <div className="divider" />
-          <BuySellForm
-            buttonColor="red"
-            buttonText="SELL"
-            orderType="ask"
-            ticker={ticker}
-            orders={sellOrders}
-            orderBook={orderBook}
-            onExecute={onExecute}
-            movieCategories={movieCategories}
-            maxSell={maxSell}
-          />
-        </div>
+      <div className="posts-container">
+        {getActiveChart(activeChart, { chartData, yDomain, labels })}
+          <div className="container-row space-between">
+            <BuySellForm
+              buttonColor="green"
+              buttonText="BUY"
+              orderType="bid"
+              ticker={ticker}
+              orders={buyOrders}
+              orderBook={orderBook}
+              onExecute={onExecute}
+              movieCategories={movieCategories}
+            />
+            <div className="divider" />
+            <BuySellForm
+              buttonColor="red"
+              buttonText="SELL"
+              orderType="ask"
+              ticker={ticker}
+              orders={sellOrders}
+              orderBook={orderBook}
+              onExecute={onExecute}
+              movieCategories={movieCategories}
+              maxSell={maxSell}
+            />
+          </div>
+      </div>
+
       <style jsx>{`
         .container {
           display: flex;
@@ -80,6 +114,16 @@ export default props => {
         .space-between {
           justify-content: space-between;
         }
+        .title {
+          color: #2d92dd;
+          font-size: 32px;
+          margin-top: 30px;
+          font-weight: lighter;
+        }
+        .posts-container {
+          margin-top: 20px;
+          fill: transparent;
+        }
         .divider {
           width: 1px;
           background: black;
@@ -87,6 +131,6 @@ export default props => {
           margin-right: 20px;
         }
       `}</style>
-    </div>
+    </Element>
   )
 }
