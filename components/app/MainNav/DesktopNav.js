5afc2e458ef5aed72cf28ce22b28574e006c1c9a
artyom <artemis.primedev@gmail.com>
2019-11-09 09:43:01 -0800

    Minor alignments; separated out search and account into a file

--- components/app/MainNav/DesktopNav.js
+++ components/app/MainNav/DesktopNav.js
@@ -3,19 +3,10 @@ import NextLink from "next/link"
 
 import {
   Button,
-  IconButton,
   Menu,
   MenuItem,
   Link,
 } from "@material-ui/core"
-
-import {
-  AccountCircle,
-  ExitToApp,
-  Search,
-} from "@material-ui/icons"
-
-import { AutoCompleteSearch } from ".."
 
 import { makeStyles } from "@material-ui/core/styles"
 import styles from './desktopNav.style.js'
@@ -26,16 +17,11 @@ const useStyles = makeStyles(styles)
 export default (props) => {
 
   const {
-    show,
     navStructure,
     handlePlaceholder,
     handleLogout,
     isLoggedIn
   } = props
-
-  if (!show) {
-    return ""
-  }
 
   const classes = useStyles()
 
@@ -46,14 +32,11 @@ export default (props) => {
       </Link>
       <div className={classes.menuBarOuter}>
         <div className={classes.menuSpacer} />
-        <DesktopMainNav 
-          navStructure={navStructure} 
-          handlePlaceholder={handlePlaceholder} 
+        <DesktopMainNav
+          navStructure={navStructure}
+          handlePlaceholder={handlePlaceholder}
           classes={classes}
         />
-      </div>
-      <div className={classes.accountOuter} >
-        <DesktopProfileArea isLoggedIn={isLoggedIn} handleLogout={handleLogout} classes={classes} />
       </div>
     </>
   )
@@ -167,89 +150,6 @@ const DesktopMainNav = (props) => {
 }
 
 
-const DesktopProfileArea = (props) => {
-
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const menuOpen = Boolean(anchorEl)
-
-  function handleMenuOpen(event) {
-    setAnchorEl(event.currentTarget)
-  }
-
-  function handleMenuClose() {
-    setAnchorEl(null)
-  }
-
-  const {
-    isLoggedIn, handleLogout, classes
-  } = props
-
-  return (<>
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <Search />
-      </div>
-      <AutoCompleteSearch
-        placeholder="Search…"
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput
-        }}
-      />
-    </div>
-    {isLoggedIn ? (
-      <>
-        <IconButton
-          aria-controls="menu"
-          aria-haspopup="true"
-          onClick={handleMenuOpen}
-        >
-          <AccountCircle style={{ fontSize: "2rem" }} />
-        </IconButton>
-        <Menu
-          anchorEl={anchorEl}
-          keepMounted
-          open={menuOpen}
-          onClose={handleMenuClose}
-          className={classes.menu}
-        >
-          <MenuItem component={CustomLink} href={"/account"}>
-            <AccountCircle />
-            <span className={classes.subMenuItemText}>Account</span>
-          </MenuItem>
-          <MenuItem component={CustomLink} href={"/portfolio"}>
-            <AccountCircle />
-            <span className={classes.subMenuItemText}>Portfolio</span>
-          </MenuItem>
-          <MenuItem onClick={handleLogout}>
-            <ExitToApp />
-            <span className={classes.subMenuItemText}>Logout</span>
-          </MenuItem>
-        </Menu>
-      </>
-    ) : (
-        <>
-          <Button
-            component={CustomLink}
-            href={"/login"}
-            color="inherit"
-            className={classes.menuButton}
-          >
-            Login
-          </Button>
-          <Button
-            component={CustomLink}
-            color="inherit"
-            variant="outlined"
-            href={"/signup"}
-            className={classes.menuButton}
-          >
-            Sign Up
-          </Button>
-        </>
-      )}
-  </>)
-}
 
 const CustomLink = React.forwardRef(
   ({ className, href, hrefAs, children, }, ref) => (
---
+++ components/app/MainNav/MobileProfileAndSearch.js
@@ -0,0 +1,118 @@
+import React from "react"
+import NextLink from "next/link"
+
+import { AutoCompleteSearch } from ".."
+
+import {
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core"
+
+import {
+  AccountCircle,
+  ExitToApp,
+  Search,
+} from "@material-ui/icons"
+
+
+import { makeStyles } from "@material-ui/core/styles"
+import styles from './desktopNav.style.js'
+
+const useStyles = makeStyles(styles)
+
+
+export default (props) => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const menuOpen = Boolean(anchorEl)
+
+  function handleMenuOpen(event) {
+    setAnchorEl(event.currentTarget)
+  }
+
+  function handleMenuClose() {
+    setAnchorEl(null)
+  }
+
+  const {
+    isLoggedIn, handleLogout
+  } = props
+
+  const classes = useStyles()
+
+  return (<>
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <Search />
+      </div>
+      <AutoCompleteSearch
+        placeholder="Search…"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput
+        }}
+      />
+    </div>
+    {isLoggedIn ? (
+      <>
+        <IconButton
+          aria-controls="menu"
+          aria-haspopup="true"
+          onClick={handleMenuOpen}
+        >
+          <AccountCircle style={{ fontSize: "2rem" }} />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={menuOpen}
+          onClose={handleMenuClose}
+          className={classes.menu}
+        >
+          <MenuItem component={CustomLink} href={"/account"}>
+            <AccountCircle />
+            <span className={classes.subMenuItemText}>Account</span>
+          </MenuItem>
+          <MenuItem component={CustomLink} href={"/portfolio"}>
+            <AccountCircle />
+            <span className={classes.subMenuItemText}>Portfolio</span>
+          </MenuItem>
+          <MenuItem onClick={handleLogout}>
+            <ExitToApp />
+            <span className={classes.subMenuItemText}>Logout</span>
+          </MenuItem>
+        </Menu>
+      </>
+    ) : (
+        <>
+          <Button
+            component={CustomLink}
+            href={"/login"}
+            color="inherit"
+            className={classes.menuButton}
+          >
+            Login
+          </Button>
+          <Button
+            component={CustomLink}
+            color="inherit"
+            variant="outlined"
+            href={"/signup"}
+            className={classes.menuButton}
+          >
+            Sign Up
+          </Button>
+        </>
+      )}
+  </>)
+}
+
+const CustomLink = React.forwardRef(
+  ({ className, href, hrefAs, children, }, ref) => (
+    <NextLink ref={ref} href={href} as={hrefAs}>
+      <a className={className}>{children}</a>
+    </NextLink>
+  )
+)
