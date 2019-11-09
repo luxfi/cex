fb804c7f5b418f4acc14157ce1d5045cddaccd62
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-08 17:03:28 -0800

    fix position of menu items

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,24 @@
 import React from "react"
 import { inject, observer } from "mobx-react"
 import { withStyles } from "@material-ui/core/styles"
-import { Container, Typography, Grid, Paper } from "@material-ui/core"
+import { Container, Typography, Grid, Box } from "@material-ui/core"
 import { NestedMenu } from "../components/app"
 
-const styles = theme => ({
-})
+const styles = theme => ({})
 
 const Menu = () => {
   return (
     <NestedMenu
       selectedKeys={["button"]}
       menus={[
-        { key: "button", label: "Button" },
-        { key: "card", label: "Card" },
-        { key: "button2", label: "Button2" },
-        { key: "card2", label: "Card2" },
+        { key: "first", label: "First" },
+        { key: "second", label: "Second" },
+        { key: "third", label: "Third" },
+        { key: "fourth", label: "Fourth" }
       ]}
     />
   )
 }
-
 
 @inject("store")
 @observer
@@ -29,11 +27,21 @@ class About extends React.Component {
     const { classes } = this.props
     return (
       <Container component="main" maxWidth="md">
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <Menu/>
-          </Grid>
-          <Grid item xs={8}>
+        <Box mt={8}>
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Box
+                style={{
+                  position: "fixed",
+                  width: "260px",
+                  // left: "32px",
+                  // top: "0px"
+                }}
+              >
+                <Menu />
+              </Box>
+            </Grid>
+            <Grid item xs={8}>
               <Typography paragraph>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -65,8 +73,40 @@ class About extends React.Component {
                 eleifend. Commodo viverra maecenas accumsan lacus vel facilisis.
                 Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
               </Typography>
+              <Typography paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Rhoncus dolor purus non enim praesent elementum facilisis leo
+                vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                hendrerit gravida rutrum quisque non tellus. Convallis convallis
+                tellus id interdum velit laoreet id donec ultrices. Odio morbi
+                quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                adipiscing bibendum est ultricies integer quis. Cursus euismod
+                quis viverra nibh cras. Metus vulputate eu scelerisque felis
+                imperdiet proin fermentum leo. Mauris commodo quis imperdiet
+                massa tincidunt. Cras tincidunt lobortis feugiat vivamus at
+                augue. At augue eget arcu dictum varius duis at consectetur
+                lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                sapien faucibus et molestie ac.
+              </Typography>
+              <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+                ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+                elementum integer enim neque volutpat ac tincidunt. Ornare
+                suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat consequat mauris. Elementum eu facilisis sed odio
+                morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                tincidunt ornare massa eget egestas purus viverra accumsan in.
+                In hendrerit gravida rutrum quisque non tellus orci ac.
+                Pellentesque nec nam aliquam sem et tortor. Habitant morbi
+                tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis.
+                Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Container>
     )
   }
