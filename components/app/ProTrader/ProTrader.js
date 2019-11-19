2d80f190c1100ed8274e733a3d0fe8eb9c4773ef
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-19 15:40:12 -0800

    fix derefences market price

--- components/app/ProTrader/ProTrader.js
+++ components/app/ProTrader/ProTrader.js
@@ -4,13 +4,13 @@ import {
   ChartCandlestickFake,
   ChartLineSeries,
   ToggleVisibleChart,
-  StockChart
-} from "../"
+  StockChart,
+} from '../'
 
-import { toJS } from "mobx"
-import { timelineLabels } from "../../../util/dateRange"
-import { Element } from "react-scroll"
-import dynamic from "next/dynamic"
+import { toJS } from 'mobx'
+import { timelineLabels } from '../../../util/dateRange'
+import { Element } from 'react-scroll'
+import dynamic from 'next/dynamic'
 import {
   Grid,
   Button,
@@ -19,14 +19,14 @@ import {
   Paper,
   Typography,
   Box,
-} from "@material-ui/core"
-import { Skeleton } from "@material-ui/lab"
+} from '@material-ui/core'
+import { Skeleton } from '@material-ui/lab'
 import NumberFormat from 'react-number-format'
-import { useState } from "react"
+import { useState } from 'react'
 import { MUIText } from '@hanzo/react'
 
 function DollarFormatCustom(props) {
-  const { inputRef, onBlur, ...other } = props;
+  const { inputRef, onBlur, ...other } = props
 
   return (
     <NumberFormat
@@ -36,18 +36,18 @@ function DollarFormatCustom(props) {
         let n = parseFloat(values.value)
         onBlur({
           target: {
-            value: isNaN(n) ? 0 : n
+            value: isNaN(n) ? 0 : n,
           },
-        });
+        })
       }}
       isNumericString
       prefix="$"
     />
-  );
+  )
 }
 
 function NumberFormatCustom(props) {
-  const { inputRef, onBlur, ...other } = props;
+  const { inputRef, onBlur, ...other } = props
 
   return (
     <NumberFormat
@@ -57,13 +57,13 @@ function NumberFormatCustom(props) {
         let n = parseFloat(values.value)
         onBlur({
           target: {
-            value: isNaN(n) ? 0 : n
+            value: isNaN(n) ? 0 : n,
           },
-        });
+        })
       }}
       isNumericString
     />
-  );
+  )
 }
 
 export default props => {
@@ -86,7 +86,7 @@ export default props => {
     marketOrderType,
     funds,
     stockName,
-    accountBalance
+    accountBalance,
   } = props
   let labels = timelineLabels()
 
@@ -103,7 +103,7 @@ export default props => {
     setMode(newValue)
   }
 
-  const executeTrade = (side) => {
+  const executeTrade = side => {
     createOrder({
       side: side,
       type: orderType,
@@ -115,15 +115,18 @@ export default props => {
   if (orderBook.isReady) {
     const { bids } = book.orderBook
     const visibleBids = bids
-        .slice()
-        .reverse()
-        .slice(0, 5)
+      .slice()
+      .reverse()
+      .slice(0, 5)
     const { asks } = book.orderBook
     const visibleAsks = bids
-        .slice()
-        .slice(0, 5)
-        .reverse()
-        
+      .slice()
+      .slice(0, 5)
+      .reverse()
+
+    const { meanPrice } = book
+    const marketPrice = Number.parseFloat(meanPrice).toFixed(2)
+
     return (
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={3}>
@@ -135,37 +138,23 @@ export default props => {
             </Tabs>
             <Box p={2}>
               <div>
-                {
-                  mode === 0 ? (
-                    <>
-                      <Typography variant='body2'>
-                        Balance
-                      </Typography>
-                      <Typography variant='h6'>
-                        ${accountBalance} USD
-                      </Typography>
-                    </>
-                  ) : (
-                      <>
-                        <Typography variant='body2'>
-                          Total Shares
-                      </Typography>
-                        <Typography variant='h6'>
-                          {maxSell}
-                        </Typography>
-                      </>
-                    )
-                }
-                <Typography variant='body2'>
-                  Last Price
-                </Typography>
-                <Typography variant='h6'>
-                  ${book.meanPrice} USD
-                </Typography>
+                {mode === 0 ? (
+                  <>
+                    <Typography variant="body2">Balance</Typography>
+                    <Typography variant="h6">${accountBalance} USD</Typography>
+                  </>
+                ) : (
+                  <>
+                    <Typography variant="body2">Total Shares</Typography>
+                    <Typography variant="h6">{maxSell}</Typography>
+                  </>
+                )}
+                <Typography variant="body2">Last Price</Typography>
+                <Typography variant="h6">${marketPrice} USD</Typography>
                 <br />
                 <MUIText
-                  label='Order Type:'
-                  variant='outlined'
+                  label="Order Type:"
+                  variant="outlined"
                   select
                   options={{
                     limit: 'Limit',
@@ -177,10 +166,12 @@ export default props => {
                 />
                 <br />
                 <MUIText
-                  label='Price:'
-                  placeholder={'$' + ((orderType === 'market') ? book.meanPrice : '100.00')}
-                  variant='outlined'
-                  value={(orderType === 'market') ? book.meanPrice : orderPrice}
+                  label="Price:"
+                  placeholder={
+                    '$' + (orderType === 'market' ? book.meanPrice : '100.00')
+                  }
+                  variant="outlined"
+                  value={orderType === 'market' ? book.meanPrice : orderPrice}
                   setValue={setOrderPrice}
                   InputProps={{
                     inputComponent: DollarFormatCustom,
@@ -190,9 +181,9 @@ export default props => {
                 />
                 <br />
                 <MUIText
-                  label='Quantity:'
-                  placeholder='100'
-                  variant='outlined'
+                  label="Quantity:"
+                  placeholder="100"
+                  variant="outlined"
                   value={orderQuantity}
                   setValue={setOrderQuantity}
                   InputProps={{
@@ -203,67 +194,61 @@ export default props => {
                 <br />
                 <Grid container>
                   <Grid item xs={6}>
-                    <Typography variant='body2'>
-                      Subtotal:
-                    </Typography>
+                    <Typography variant="body2">Subtotal:</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant='body2' align='right'>
+                    <Typography variant="body2" align="right">
                       ${(orderPrice * orderQuantity).toFixed(2)}
                     </Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant='body2'>
-                      Fee:
-                    </Typography>
+                    <Typography variant="body2">Fee:</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant='body2' align='right'>
+                    <Typography variant="body2" align="right">
                       ${(orderPrice * orderQuantity * 0.005).toFixed(2)}
                     </Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant='body2'>
-                      Total:
-                    </Typography>
+                    <Typography variant="body2">Total:</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant='body2' align='right'>
+                    <Typography variant="body2" align="right">
                       ${(orderPrice * orderQuantity * 1.005).toFixed(2)}
                     </Typography>
                   </Grid>
                 </Grid>
                 <br />
               </div>
-              {
-                mode === 0 && (
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    fullWidth
-                    onClick={() => executeTrade('bid')}
-                  >
-                    Buy
-                  </Button>
-                )
-              }
-              {
-                mode === 1 && (
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    fullWidth
-                    onClick={() => executeTrade('ask')}
-                  >
-                    Sell
-                  </Button>
-                )
-              }
+              {mode === 0 && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={() => executeTrade('bid')}
+                >
+                  Buy
+                </Button>
+              )}
+              {mode === 1 && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={() => executeTrade('ask')}
+                >
+                  Sell
+                </Button>
+              )}
             </Box>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={9}>
-          <StockChart stock={stock} stockName={stockName} connected={connected} />
+          <StockChart
+            stock={stock}
+            stockName={stockName}
+            connected={connected}
+          />
         </Grid>
         <Grid item xs={12}>
           <Paper>
@@ -276,7 +261,7 @@ export default props => {
                   Quantity
                 </Grid>
               </Grid>
-              {visibleAsks.map((ask, i ) =>
+              {visibleAsks.map((ask, i) => (
                 <Grid container style={{ color: 'red' }} key={i}>
                   <Grid item xs={6}>
                     ${parseFloat(ask[0]).toFixed(2)}
@@ -285,9 +270,8 @@ export default props => {
                     {parseFloat(ask[1]).toFixed(0)}
                   </Grid>
                 </Grid>
-              )
-              }
-              {visibleBids.map((bid, i) =>
+              ))}
+              {visibleBids.map((bid, i) => (
                 <Grid container style={{ color: 'green' }} key={i}>
                   <Grid item xs={6}>
                     ${parseFloat(bid[0]).toFixed(2)}
@@ -296,21 +280,19 @@ export default props => {
                     {parseFloat(bid[1]).toFixed(0)}
                   </Grid>
                 </Grid>
-              )
-              }
+              ))}
             </Box>
           </Paper>
         </Grid>
       </Grid>
     )
-  }
-  else {
+  } else {
     return (
-        <React.Fragment>
-            <Skeleton width="25%" />
-            <Skeleton height={32} width="15%" />
-            <Skeleton variant="rect" height={300} />
-        </React.Fragment>
-    );
+      <React.Fragment>
+        <Skeleton width="25%" />
+        <Skeleton height={32} width="15%" />
+        <Skeleton variant="rect" height={300} />
+      </React.Fragment>
+    )
   }
 }
