73f68c3adb6c7661cdba1d77477aa3c51ad68cf3
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-21 01:15:26 -0800

    save work on chat

--- components/trade/index.js
+++ components/trade/index.js
@@ -1,5 +1,6 @@
 export { default as AboutSection } from "./AboutSection"
 export { default as BuySellWidget } from './BuySellWidget'
+export { default as EarningsSection } from './EarningsSection'
 export { default as FinancialsSection } from './FinancialsSection'
 export { default as HistorySection } from './HistorySection'
 export { default as TradeView } from './TradeView'
