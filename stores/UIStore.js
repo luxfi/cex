bb2e7bed769b9c0c7c647b3f052c3688bea7dbad
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-20 18:32:55 -0800

    clean up files

--- stores/UIStore.js
+++ stores/UIStore.js
@@ -1,56 +1,56 @@
-import { action, observable, computed } from "mobx"
+import { action, observable, computed } from 'mobx'
 
 export default class UIStore {
   @observable snackBarOpen = false
   @observable snackBarVariant = 'error'
   @observable snackBarMessage = ''
 
-    @observable drawers = {
-        left: false,
-        right: false
-    }
-  
-    @observable modal = {
-        open: false,
-        title: null,
-        body: null
-    }
+  @observable drawers = {
+    left: false,
+    right: false,
+  }
 
-    @action setLeftDrawerOpen = open => {
-        this.drawers.left = open
-    }
+  @observable modal = {
+    open: false,
+    title: null,
+    body: null,
+  }
 
-    @action setErrorMessage = message => {
-      this.setSnackBarMessage(message, "error")
-    }
-  
-    @action setSuccessMessage = message => {
-      this.setSnackBarMessage(message, "success")
-    }
-  
-    @action setSnackBarMessage = (message, variant) => {
-      this.snackBarVariant = variant
-      this.snackBarMessage = message
-      this.snackBarOpen = true
-    }
+  @action setLeftDrawerOpen = open => {
+    this.drawers.left = open
+  }
 
-    @action setLeftDrawerOpen = open => {
-        this.drawers.left = open
-    }
+  @action setErrorMessage = message => {
+    this.setSnackBarMessage(message, 'error')
+  }
 
-    @action setRightDrawerOpen = open => {
-        this.drawers.right = open
-    }
+  @action setSuccessMessage = message => {
+    this.setSnackBarMessage(message, 'success')
+  }
 
-    @action openModal(title, body) {
-        this.modal.open = true
-        this.modal.title = title
-        this.modal.body = body
-    }
+  @action setSnackBarMessage = (message, variant) => {
+    this.snackBarVariant = variant
+    this.snackBarMessage = message
+    this.snackBarOpen = true
+  }
 
-    @action closeModal() {
-        this.modal.open = false
-        this.modal.title = null
-        this.modal.body = null
-    }
+  @action setLeftDrawerOpen = open => {
+    this.drawers.left = open
+  }
+
+  @action setRightDrawerOpen = open => {
+    this.drawers.right = open
+  }
+
+  @action openModal(title, body) {
+    this.modal.open = true
+    this.modal.title = title
+    this.modal.body = body
+  }
+
+  @action closeModal() {
+    this.modal.open = false
+    this.modal.title = null
+    this.modal.body = null
+  }
 }
