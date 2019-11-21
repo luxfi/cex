6c1b17befeb95dfaaef1bae359e08b2620417f72
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-21 02:46:35 -0800

    make films clickable

--- components/trade/OtherFilmsTrading/OtherFilmsTrading.js
+++ components/trade/OtherFilmsTrading/OtherFilmsTrading.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import { Grid, Typography, Box } from '@material-ui/core'
+import Router from 'next/Router'
 
 const OtherFilmsTrading = ({ movies }) => {
-  const selectedMovies = movies.slice(0,5)
+  const selectedMovies = movies.slice(0, 5)
   return (
     <Box mt={6} mb={6.5}>
       <Typography component="div" variant="subtitle2" gutterBottom>
@@ -10,23 +11,44 @@ const OtherFilmsTrading = ({ movies }) => {
           OTHER FILMS TRADING
         </Box>
       </Typography>
-      <Grid
-        container
-        justify="flex-start"
-        spacing={1}
-      >
-        {selectedMovies.map((movie, i) => (
-          <Grid key={i} item xs>
-            <Typography component="div">
-              <Box fontWeight="fontWeightBold">
-                <img src={movie.posterImg} width="100%" />
-              </Box>
-            </Typography>
-          </Grid>
-        ))}
+      <Grid container justify="flex-start" spacing={1}>
+        {selectedMovies.map((movie, i) => {
+          return (
+            <Grid key={i} item xs>
+              <Typography component="div">
+                <Box
+                  fontWeight="fontWeightBold"
+                  onClick={() => {
+                    Router.push(`/trade/${movie.movieSlug}`)
+                  }}
+                >
+                  <img src={movie.posterImg} width="100%" />
+                </Box>
+              </Typography>
+            </Grid>
+          )
+        })}
       </Grid>
     </Box>
   )
 }
 
 export default OtherFilmsTrading
+
+// <Grid item>
+//   <Button
+//     className={classes.investButton}
+//     size="large"
+//     startIcon={<MonetizationOnIcon />}
+//   >
+//     <NextLink href={hrefLink}>
+//       <Typography
+//         variant="body2"
+//         className={classes.watchTrailerButtonText}
+//       >
+//         INVEST IN {movie.name}
+//       </Typography>
+//     </NextLink>
+//   </Button>
+//   const hrefLink = '/film/' + movie.movieSlug
+// </Grid>
