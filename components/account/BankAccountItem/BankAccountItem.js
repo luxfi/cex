1189b16607c44b62ef601d7ca0557c7047d78300
Everett <newetown@hotmail.com>
2019-11-20 16:12:27 -0800

    Initial WIP, needs a bit of cleanup

---
+++ components/account/BankAccountItem/BankAccountItem.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {
+  Button,
+  Divider,
+  Grid,
+  Typography
+ } from '@material-ui/core'
+//  import {AddIcon} from "@material-ui/icons/Backup"
+
+export default props => {
+  const {
+    accountName,
+    accountNumber,
+    accountType,
+    subtype,
+    institution,
+    removeAccount
+  } = props
+
+  let iconType
+  switch (accountType) {
+    case 'depository':
+      iconType = 'account_balance'
+      break
+    case 'credit':
+      iconType = 'credit_card'
+      break
+    default:
+      iconType = 'account_balance'
+      break
+  }
+
+  // accountType is 'bank' or 'cc'
+
+  return (
+    <Grid item xs={12}>
+      <Grid container direction="column" spacing={0}>
+        <Divider style={{ marginBottom: '15px' }} />
+        <Grid container>
+          <Grid item xs={3}>
+          <i className="material-icons" style={{ fontSize: '60px' }}>
+            {iconType}
+          </i>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h5">
+              {accountName} - {accountNumber}
+            </Typography>
+            <Typography variant="h6">
+              {institution} {subtype} ****{accountNumber}
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              onClick={removeAccount}
+              variant="outlined"
+              style={{ height: '55px' }}
+            >
+              Remove
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
--- pages/account/index.js
+++ pages/account/index.js
@@ -10,10 +10,16 @@ import {
   Divider,
   Button,
   TextField,
+  Typography,
 } from "@material-ui/core"
-import Typography from "@material-ui/core/Typography"
 
-import { AddPaymentMethodForm } from "../../components/account"
+import { 
+  AccountSection ,
+  AddPaymentMethodForm,
+  BankAccountItem,
+  ManageFunds,
+  BalanceHistoryItem
+} from "../../components/account"
 import { CustomLink } from '../../components/app'
 
 import { googlePageView } from '../../util/generic'
@@ -31,6 +37,7 @@ class Account extends React.Component {
 
   componentDidMount() {
     googlePageView()
+    this.props.store.userStore.loadBalanceHistory()
   }
 
   render() {
@@ -39,6 +46,8 @@ class Account extends React.Component {
     const {
       setValue,
       account,
+      formattedAccounts,
+      balanceHistory,
       addPaymentMethod,
       isValidNewPaymentMethod,
       validNewPaymentMethodName,
@@ -46,7 +55,7 @@ class Account extends React.Component {
       validateNewPaymentMethodName,
       validateNewPaymentMethodPublicToken,
       validateNewPaymentMethodMetadata,
-      addBalance,
+      handleFunds
     } = userStore
 
     const setErrorMessage = message => {
@@ -56,103 +65,165 @@ class Account extends React.Component {
 
     return (
       <Container maxWidth="lg" style={{ marginTop: '70px', marginBottom: '30px' }}>
-        <Grid
-          container
-          spacing={3}
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <Grid item xs={12} sm={4}>
-            <Card>
-              <CardContent>
-                <AddPaymentMethodForm
-                  addPaymentMethod={addPaymentMethod.bind(userStore)}
-                  validateNewPaymentMethodName={validateNewPaymentMethodName.bind(userStore)}
-                  validateNewPaymentMethodPublicToken={validateNewPaymentMethodPublicToken.bind(userStore)}
-                  validateNewPaymentMethodMetadata={validateNewPaymentMethodMetadata.bind(userStore)}
-                  isValidNewPaymentMethod={isValidNewPaymentMethod}
-                  validNewPaymentMethodName={validNewPaymentMethodName}
-                  validNewPaymentMethodPublicToken={validNewPaymentMethodPublicToken}
-                  setValue={setValue.bind(userStore)}
-                  setErrorMessage={setErrorMessage}
-                  refreshSession={() => { userStore.loadSession() }}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-          {
-            !!account && !!account.paymentMethods ?
-              <Grid item xs={12} sm={8}>
+        <AccountSection title="Linked Accounts">
+          <Grid container>
+            <Grid item xs={8}>
+              <Grid container direction="column">
                 {
-                  account.paymentMethods.map((m, i) => {
+                  formattedAccounts.map((a, i) => {
+                    if (a.name === 'ESX') return null
                     return (
-                      <Card key={`m+${i}`} style={{ marginBottom: 20 }}>
-                        <CardContent>
-                          <Typography variant="body1">
-                            {m.name}
-                          </Typography>
-                          <Typography variant="body2">
-                            {(m.Inputs && m.Inputs.metadata.institution) ? m.Inputs.metadata.institution.name : ""}
-                          </Typography>
-                          <Grid container>
-                            <Grid item xs={4}>
-                              <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="newFunds"
-                                label="Add Funds"
-                                id="add-new-funds"
-                                value={this.state[`${m.name}-newFunds`] || ""}
-                                onChange={evt => {
-                                  this.setState({
-                                    [`${m.name}-newFunds`]: evt.target.value,
-                                    [`${m.name}-newFunds-error`]: null
-                                  })
-                                }}
-                                helperText={this.state[`${m.name}-newFunds-error`]}
-                                error={this.state[`${m.name}-newFunds-error`] && this.state[`${m.name}-newFunds-error`].length > 0}
-                              />
-                            </Grid>
-                          </Grid>
-                          <Button
-                            variant="outlined"
-                            onClick={
-                              () => {
-                                userStore.addBalance(
-                                  this.state[`${m.name}-newFunds`],
-                                  () => { this.setState({ [`${m.name}-newFunds`]: "" }) },
-                                  () => {
-                                    this.setState({
-                                      [`${m.name}-newFunds`]: "",
-                                      [`${m.name}-newFunds-error`]: `Error adding funds from ${m.name}`
-                                    })
-                                  }
-                                )
-                              }
-                            }
-                          >
-                            Add Funds
-                          </Button>
-                          <Divider variant="inset" style={{ marginTop: '10px' }} />
-                        </CardContent>
-                      </Card>
+                      <BankAccountItem
+                        key={`account_${i}`}
+                        accountName={a.name}
+                        accountNumber={a.account.mask}
+                        accountType={a.account.type}
+                        institution={a.institution.name}
+                        subtype={a.account.subtype}
+                        removeAccount={() => { console.log('Removing the account!', a.id) }}
+                      />
                     )
                   })
                 }
               </Grid>
-              : null
-          }
-          <Grid item xs={12} sm={4}>
-            <Link component={CustomLink} href="/account/kyc">
-              Check your identify verification status
-            </Link>
+            </Grid>
+            <Grid item xs={4}>
+              <Grid container direction="column" alignItems="center">
+                <ManageFunds
+                  accountList={formattedAccounts}
+                  handleFunds={handleFunds.bind(userStore)}
+                />
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
+        </AccountSection>
+        <AccountSection title="Complete Transfers">
+          <Grid container>
+            <Grid item xs={8}>
+              <Grid container direction="column">
+                {
+                  balanceHistory && balanceHistory.length > 0
+                  ?
+                    balanceHistory.map((r, i) => {
+                      return (
+                        <BalanceHistoryItem
+                          key={`history_${i}`}
+                          name={r.name}
+                          amount={r.amount}
+                          date={r.date}
+                          deposit={r.deposit}
+                        />
+                      )
+                    })
+                  :
+                   <Typography variant="h6">No transfers have been made yet!</Typography>
+                }
+              </Grid>
+            </Grid>
+          </Grid>
+        </AccountSection>
       </Container>
     )
+
+    // return (
+    //   <Container maxWidth="lg" style={{ marginTop: '70px', marginBottom: '30px' }}>
+    //     <Grid
+    //       container
+    //       spacing={3}
+    //       direction="column"
+    //       justify="center"
+    //       alignItems="center"
+    //     >
+    //       <Grid item xs={12} sm={4}>
+    //         <Card>
+    //           <CardContent>
+    //             <AddPaymentMethodForm
+    //               addPaymentMethod={addPaymentMethod.bind(userStore)}
+    //               validateNewPaymentMethodName={validateNewPaymentMethodName.bind(userStore)}
+    //               validateNewPaymentMethodPublicToken={validateNewPaymentMethodPublicToken.bind(userStore)}
+    //               validateNewPaymentMethodMetadata={validateNewPaymentMethodMetadata.bind(userStore)}
+    //               isValidNewPaymentMethod={isValidNewPaymentMethod}
+    //               validNewPaymentMethodName={validNewPaymentMethodName}
+    //               validNewPaymentMethodPublicToken={validNewPaymentMethodPublicToken}
+    //               setValue={setValue.bind(userStore)}
+    //               setErrorMessage={setErrorMessage}
+    //               refreshSession={() => { userStore.loadSession() }}
+    //             />
+    //           </CardContent>
+    //         </Card>
+    //       </Grid>
+    //       {
+    //         !!account && !!account.paymentMethods ?
+    //           <Grid item xs={12} sm={8}>
+    //             {
+    //               account.paymentMethods.map((m, i) => {
+    //                 return (
+    //                   <Card key={`m+${i}`} style={{ marginBottom: 20 }}>
+    //                     <CardContent>
+    //                       <Typography variant="body1">
+    //                         {m.name}
+    //                       </Typography>
+    //                       <Typography variant="body2">
+    //                         {(m.Inputs && m.Inputs.metadata.institution) ? m.Inputs.metadata.institution.name : ""}
+    //                       </Typography>
+    //                       <Grid container>
+    //                         <Grid item xs={4}>
+    //                           <TextField
+    //                             variant="outlined"
+    //                             margin="normal"
+    //                             required
+    //                             fullWidth
+    //                             name="newFunds"
+    //                             label="Add Funds"
+    //                             id="add-new-funds"
+    //                             value={this.state[`${m.name}-newFunds`] || ""}
+    //                             onChange={evt => {
+    //                               this.setState({
+    //                                 [`${m.name}-newFunds`]: evt.target.value,
+    //                                 [`${m.name}-newFunds-error`]: null
+    //                               })
+    //                             }}
+    //                             helperText={this.state[`${m.name}-newFunds-error`]}
+    //                             error={this.state[`${m.name}-newFunds-error`] && this.state[`${m.name}-newFunds-error`].length > 0}
+    //                           />
+    //                         </Grid>
+    //                       </Grid>
+    //                       <Button
+    //                         variant="outlined"
+    //                         onClick={
+    //                           () => {
+    //                             addBalance(
+    //                               this.state[`${m.name}-newFunds`],
+    //                               () => { this.setState({ [`${m.name}-newFunds`]: "" }) },
+    //                               () => {
+    //                                 this.setState({
+    //                                   [`${m.name}-newFunds`]: "",
+    //                                   [`${m.name}-newFunds-error`]: `Error adding funds from ${m.name}`
+    //                                 })
+    //                               }
+    //                             )
+    //                           }
+    //                         }
+    //                       >
+    //                         Add Funds
+    //                       </Button>
+    //                       <Divider variant="inset" style={{ marginTop: '10px' }} />
+    //                     </CardContent>
+    //                   </Card>
+    //                 )
+    //               })
+    //             }
+    //           </Grid>
+    //           : null
+    //       }
+    //       <Grid item xs={12} sm={4}>
+    //         <Link component={CustomLink} href="/account/kyc">
+    //           Check your identify verification status
+    //         </Link>
+    //       </Grid>
+    //     </Grid>
+    //   </Container>
+    // )
   }
 }
 
