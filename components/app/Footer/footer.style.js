98c5bee9e1a34ed1d1bf1fc2920ceba1c55aa805
artyom <artemis.primedev@gmail.com>
2019-11-21 08:32:22 -0800

    Fixed Android button art!; correct layout

--- components/app/Footer/footer.style.js
+++ components/app/Footer/footer.style.js
@@ -14,77 +14,158 @@ export default theme => {
       },
     },
 
+    logoGridItem: {
+      order: 0,
+      [theme.breakpoints.down("md")]: {
+        display: "none"
+      },
+    },
+
+    navGridItem: {
+      order: 2,
+      [theme.breakpoints.up("lg")]: {
+        order: 1,
+      },
+      [theme.breakpoints.down("sm")]: {
+        order: 3,
+      },
+    },
+
+    socialAndAppGridItem: {
+      order: 2,
+    },
+
     logoImg: {
       display: "block",
       position: "relative",
-      top: "-10px"
+      top: "-10px",
+      [theme.breakpoints.only("md")]: {
+        height: "32px !important"
+      },
+
     },
+
+    socialGridItem: {
+      order: 1,
+      marginBottom: theme.spacing(2),
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      [theme.breakpoints.up("lg")]: {
+        justifyContent: "flex-end",
+        order: 0, // arbitrary, doesn't factor into main ordering
+      },
+    },
+
     socialIconRow: {
       display: "flex",
       flexDirection: "row",
-      justifyContent: "left",
+      justifyContent: "flex-end",
       alignItems: "center",
     },
     socialIcon: {
       display: "block",
       padding: 0,
-      paddingRight: theme.spacing(2)
+      paddingRight: theme.spacing(3),
+      "&:last-child": {
+        paddingRight: 0
+      }
     },
 
-    footerColumnsOuter: {
+    navOuter: {
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
       marginBottom: theme.spacing(3),
       [theme.breakpoints.down("sm")]: {
-        flexDirection: "column-reverse",
         marginBottom: theme.spacing(1),
       }
     },
-    footerColumn: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "align-left",
-      [theme.breakpoints.down("xs")]: {
-        alignItems: "center",
-      }
-    },
-    footerColumnInner: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "align-left",
-      marginBottom: theme.spacing(2),
-      [theme.breakpoints.down("xs")]: {
-        alignItems: "center",
-      }
-    },
-    appArea: {
-      display: "flex",
-      flexDirection: "column",
+    navGridContainer: {
+      flexDirection: "row",
       justifyContent: "center",
-      [theme.breakpoints.down("sm")]: {
-        marginBottom: theme.spacing(3)
+      [theme.breakpoints.down("xs")]: {
+        alignItems: "stretch",
       }
     },
-    appStoreButtons: {
+    navSectionGridItem: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      [theme.breakpoints.down("md")]: {
+        alignItems: "center",
+      },
+      marginBottom: theme.spacing(2),
+    },
+    navSectionTitle: {
+      fontSize: "1.3rem"
+    },
+    navSectionHR: {
+      display: "none",
+      [theme.breakpoints.only("xs")]: {
+        margin: 0,
+        marginBottom: "4px",
+        display: "block",
+        width: "17%",
+        borderBottom: "none",
+        opacity: "0.7"
+      }
+    },
 
+    appStoreGridItem: {
+      order: 3,
+      display: "flex",
+      flexDirection: "row",
+      [theme.breakpoints.down("sm")]: {
+        marginBottom: theme.spacing(3),
+        order: 2
+      },
+      [theme.breakpoints.up("lg")]: {
+        justifyContent: "flex-end",
+      },
+      [theme.breakpoints.down("md")]: {
+        justifyContent: "center",
+      },
     },
-    appleAppStore: {
-      width: "100px",
+    appStoreOuter: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      [theme.breakpoints.up("lg")]: {
+        alignItems: "flex-end",
+      },
     },
-    androidAppStore: {
-      width: "120px",
+    appStoreTitle: {
+      fontSize: "0.9rem",
+      marginBottom: theme.spacing(1)
+    },
+    appStoreButtonsOuter: {
+    },
+    appStoreApple: {
+      width: "110px",
+      paddingRight: theme.spacing(1)
     },
 
+    copyrightGridItem: {
+      order: 4,
+    },
 
     copyrightOuter: {
       display: "flex",
       flexDirection: "row",
       justifyContent: "center",
+      marginTop: theme.spacing(2),
+      [theme.breakpoints.up("lg")]: {
+        justifyContent: "flex-start",
+        marginTop: theme.spacing(1),
+      },
       "& p": {
         marginRight: theme.spacing(1)
-      }
+      },
+      marginBottom: theme.spacing(2),
     },
+
     "@global": {
       ul: {
         margin: 0,
