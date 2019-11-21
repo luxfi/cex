5d939819b8a7164457c38aed625e3b91b795ee48
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-20 18:28:42 -0800

    undo editor changes

--- components/app/CustomSnackbar/CustomSnackbar.js
+++ components/app/CustomSnackbar/CustomSnackbar.js
@@ -1,74 +1,74 @@
-import React from "react"
-import { inject, observer } from "mobx-react"
-import PropTypes from "prop-types"
-import clsx from "clsx"
-import CheckCircleIcon from "@material-ui/icons/CheckCircle"
-import ErrorIcon from "@material-ui/icons/Error"
-import InfoIcon from "@material-ui/icons/Info"
-import CloseIcon from "@material-ui/icons/Close"
-import { amber, green } from "@material-ui/core/colors"
-import IconButton from "@material-ui/core/IconButton"
-import Snackbar from "@material-ui/core/Snackbar"
-import SnackbarContent from "@material-ui/core/SnackbarContent"
-import WarningIcon from "@material-ui/icons/Warning"
-import { makeStyles } from "@material-ui/core/styles"
+import React from 'react'
+import { inject, observer } from 'mobx-react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import ErrorIcon from '@material-ui/icons/Error'
+import InfoIcon from '@material-ui/icons/Info'
+import CloseIcon from '@material-ui/icons/Close'
+import { amber, green } from '@material-ui/core/colors'
+import IconButton from '@material-ui/core/IconButton'
+import Snackbar from '@material-ui/core/Snackbar'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
+import WarningIcon from '@material-ui/icons/Warning'
+import { makeStyles } from '@material-ui/core/styles'
 
 const variantIcon = {
-    success: CheckCircleIcon,
-    warning: WarningIcon,
-    error: ErrorIcon,
-    info: InfoIcon
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
 }
 
 const useStyles1 = makeStyles(theme => ({
-    success: {
-        backgroundColor: green[600]
-    },
-    error: {
-        backgroundColor: theme.palette.error.dark
-    },
-    info: {
-        backgroundColor: theme.palette.primary.main
-    },
-    warning: {
-        backgroundColor: amber[700]
-    },
-    icon: {
-        fontSize: 20
-    },
-    iconVariant: {
-        opacity: 0.9,
-        marginRight: theme.spacing(1)
-    },
-    message: {
-        display: "flex",
-        alignItems: "center"
-    }
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 }))
 
 function MySnackbarContentWrapper(props) {
-    const classes = useStyles1()
-    const { className, message, onClose, variant, ...other } = props
-    const Icon = variantIcon[variant]
+  const classes = useStyles1()
+  const { className, message, onClose, variant, ...other } = props
+  const Icon = variantIcon[variant]
 
-    return (
-        <SnackbarContent
-            className={clsx(classes[variant], className)}
-            aria-describedby="client-snackbar"
-            message={
-                <span id="client-snackbar" className={classes.message}>
-                    <Icon className={clsx(classes.icon, classes.iconVariant)} />
-                    {message}
-                </span>
-            }
-            action={[
-                <IconButton key="close" aria-label="close" onClick={onClose}>
-                    <CloseIcon className={classes.icon} />
-                </IconButton>
-            ]}
-            {...other}
-        />
-    )
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton key="close" aria-label="close" onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  )
 }
 
 // MySnackbarContentWrapper.propTypes = {
@@ -79,48 +79,48 @@ function MySnackbarContentWrapper(props) {
 // }
 
 const useStyles2 = makeStyles(theme => ({
-    margin: {
-        margin: theme.spacing(1)
-    }
+  margin: {
+    margin: theme.spacing(1),
+  },
 }))
 
 function CustomizedSnackbars({ setOpen, open, variant, message }) {
-    const classes = useStyles2()
-    // const [open, setOpen] = React.useState(false)
+  const classes = useStyles2()
+  // const [open, setOpen] = React.useState(false)
 
-    const handleClick = () => {
-        setOpen(true)
+  const handleClick = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
     }
 
-    const handleClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return
-        }
+    setOpen(false)
+  }
 
-        setOpen(false)
+  const defaultMessage = () => {
+    switch (variant) {
+      case 'error':
+        return 'Whoops, something went wrong'
+      case 'success':
+        return 'This is a success message!'
+      default:
+        return 'Whoops, something went wrong'
     }
+  }
 
-    const defaultMessage = () => {
-        switch (variant) {
-            case "error":
-                return "Whoops, something went wrong"
-            case "success":
-                return "This is a success message!"
-            default:
-                return "Whoops, something went wrong"
-        }
-    }
-
-    return (
-        <div>
-            {/* <Button
+  return (
+    <div>
+      {/* <Button
         variant="outlined"
         className={classes.margin}
         onClick={handleClick}
       >
         Open success snackbar
       </Button> */}
-            {/* <Snackbar
+      {/* <Snackbar
         anchorOrigin={{
           vertical: "bottom",
           horizontal: "left"
@@ -135,62 +135,62 @@ function CustomizedSnackbars({ setOpen, open, variant, message }) {
           message="This is a success message!"
         />
       </Snackbar> */}
-            <Snackbar
-                anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left"
-                }}
-                open={open}
-                autoHideDuration={3000}
-                onClose={handleClose}
-            >
-                <MySnackbarContentWrapper
-                    onClose={handleClose}
-                    variant={variant}
-                    className={classes.margin}
-                    message={message || defaultMessage()}
-                />
-            </Snackbar>
-            {/* <MySnackbarContentWrapper
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      >
+        <MySnackbarContentWrapper
+          onClose={handleClose}
+          variant={variant}
+          className={classes.margin}
+          message={message || defaultMessage()}
+        />
+      </Snackbar>
+      {/* <MySnackbarContentWrapper
         variant="warning"
         className={classes.margin}
         message="This is a warning message!"
       /> */}
-            {/* <MySnackbarContentWrapper
+      {/* <MySnackbarContentWrapper
         variant="info"
         className={classes.margin}
         message="This is an information message!"
       /> */}
-            {/* <MySnackbarContentWrapper
+      {/* <MySnackbarContentWrapper
         variant="success"
         className={classes.margin}
         message="This is a success message!"
       /> */}
-        </div>
-    )
+    </div>
+  )
 }
 
-@inject("store")
+@inject('store')
 @observer
 export default class MySnackBar extends React.Component {
-    render() {
-        const { store } = this.props
-        const setOpen = val => {
-            store.uiStore.snackBarOpen = val
-        }
-
-        const open = store.uiStore.snackBarOpen
-        const { snackBarMessage } = store.uiStore
-        const { snackBarVariant } = store.uiStore
-
-        return (
-            <CustomizedSnackbars
-                store={store}
-                setOpen={setOpen}
-                open={open}
-                message={snackBarMessage}
-                variant={snackBarVariant}
-            />
-        )
+  render() {
+    const { store } = this.props
+    const setOpen = val => {
+      store.uiStore.snackBarOpen = val
     }
+
+    const open = store.uiStore.snackBarOpen
+    const { snackBarMessage } = store.uiStore
+    const { snackBarVariant } = store.uiStore
+
+    return (
+      <CustomizedSnackbars
+        store={store}
+        setOpen={setOpen}
+        open={open}
+        message={snackBarMessage}
+        variant={snackBarVariant}
+      />
+    )
+  }
 }
