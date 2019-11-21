ae2f55d71f0a2d7562a5905c36fb5ad896707b06
Everett <newetown@hotmail.com>
2019-11-21 11:37:42 -0800

    Few more QOL updates

--- components/account/AccountSection/AccountSection.js
+++ components/account/AccountSection/AccountSection.js
@@ -5,11 +5,12 @@ import Grid from '@material-ui/core/Grid'
 export default props => {
   const { 
     title,
-    children
+    children,
+    style
   } = props
 
   return (
-      <Grid container direction="column">
+      <Grid container direction="column" style={style || {}}>
         <Grid item xs={12}>
           <Typography variant="h4" gutterBottom>
             {title}
