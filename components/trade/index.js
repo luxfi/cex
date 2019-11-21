165242d4a81176189d9f306324c87629b8a84c1d
TylerJNewman <tylerjnewman18@gmail.com>
2019-11-20 21:19:33 -0800

    create skeleton

--- components/trade/index.js
+++ components/trade/index.js
@@ -1,4 +1,5 @@
 export { default as AboutSection } from "./AboutSection"
 export { default as BuySellWidget } from './BuySellWidget'
 export { default as FinancialsSection } from './FinancialsSection'
+export { default as HistorySection } from './HistorySection'
 export { default as TradeView } from './TradeView'
