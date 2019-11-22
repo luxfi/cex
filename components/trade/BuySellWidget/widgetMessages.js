294d9fb031ec1367a621857b8d10c1b484b46e72
Tyler Newman <tylerjnewman18@gmail.com>
2019-11-22 15:42:17 -0800

    implement logic for not enough funds on purchase

--- components/trade/BuySellWidget/widgetMessages.js
+++ components/trade/BuySellWidget/widgetMessages.js
@@ -1,12 +1,19 @@
 import { pluralize } from '../../../util/generic'
 
-const errorNotEnoughFunds = ({ total, shares, ticker }) => {
-  const title = "Not Enough Buying Power"
-  const p1 = `You don’t have enough buying power to buy 1 share of ${ticker}`
-  const p2 = `Please deposit $${total} to purchase ${shares} ${pluralize(shares, 'share')} at market price (5% collar included).`
-  const p3 = "Market orders on ESX are placed as limit orders up to 5% above the market price in order to protect customers from spending more than they have in their ESX account."
+const errorNotEnoughFunds = (total, shares, ticker) => {
+  const title = 'Not Enough Buying Power'
+  const p1 = `You don’t have enough buying power to buy ${shares} ${pluralize(
+    shares,
+    'share',
+  )} of ${ticker}`
+  const p2 = `Please deposit $${total} to purchase ${shares} ${pluralize(
+    shares,
+    'share',
+  )} at market price (5% collar included).`
+  const p3 =
+    'Market orders on ESX are placed as limit orders up to 5% above the market price in order to protect customers from spending more than they have in their ESX account.'
   const body = [p1, p2, p3]
-  return {title, body}
+  return { title, body }
 }
 
 const VALID_SHARES_ERROR = `Error
