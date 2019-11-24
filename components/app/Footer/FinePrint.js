68274c0db29d9df3616e5bcfb68f9d05dd4b3932
Artem Ash <artemisprimedev@gmail.com>
2019-11-24 07:15:37 -0800

    Footer responsive as per Republic.co model

---
+++ components/app/Footer/FinePrint.js
@@ -0,0 +1,13 @@
+export default (props) => (
+	<div className={props.className}>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.Ut malesuada tempor turpis, vitae lacinia tellus consectetur imperdiet.Vestibulum tincidunt placerat dui eu dictum.Duis vehicula ante lacus, id facilisis nulla ultricies eget.Duis dapibus interdum metus, non feugiat purus porttitor eu.Pellentesque cursus lorem eros, eu aliquam libero auctor ac.In ut hendrerit nunc, in vulputate sapien.Maecenas vestibulum venenatis ante quis mollis.Sed lobortis orci nulla, sed ornare mi volutpat at. 
+		</p>
+		<p>
+			Integer in diam velit. Integer rhoncus nibh eget odio hendrerit, ut tincidunt massa elementum.Vivamus viverra, nibh a condimentum dictum, diam augue commodo ante, sed malesuada leo sapien at eros.Nulla in odio et neque faucibus sagittis lacinia venenatis lectus.Nullam suscipit consequat facilisis.In laoreet sem sed ante efficitur, sed tempor sem sagittis.Nulla aliquet elit ac ex auctor consequat.Curabitur auctor neque vel turpis tempor tristique.Maecenas aliquam tellus diam, sit amet suscipit odio mollis sit amet.Etiam malesuada, lectus a hendrerit consectetur, tellus nisl lacinia ex, vel fringilla orci nunc non sem.Suspendisse tincidunt ante malesuada, tincidunt ex in, rutrum sapien.Duis suscipit velit id nisi consequat scelerisque.Phasellus suscipit eu nisl sit amet placerat.Sed vestibulum posuere erat ac dignissim.Nam pulvinar, mi et interdum vulputate, erat arcu tincidunt lectus, vel eleifend lectus orci ut justo.
+		</p>
+		<p>
+		Sed dapibus iaculis orci, id commodo ex suscipit eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Donec pulvinar ac mauris eu eleifend.Donec bibendum, elit eu bibendum ullamcorper, justo eros molestie ligula, eu maximus orci tortor vel sapien.Duis tortor ante, scelerisque eget justo eu, eleifend malesuada orci.Ut gravida lectus ut nisi fringilla, quis luctus magna faucibus.Maecenas efficitur, purus non efficitur laoreet, odio turpis interdum elit, non pharetra ex neque id orci.Sed interdum lacus ipsum, sed pretium augue dictum a.Cras ac blandit enim, nec tempus dolor.Pellentesque sed sem metus.Mauris fringilla ex pharetra elit commodo, at porttitor orci sagittis.Vestibulum tempus turpis sapien, ac euismod arcu viverra eget.Donec ullamcorper euismod dolor.Pellentesque malesuada mi sed odio malesuada, et blandit nisl tincidunt.In pharetra luctus nisi sit amet euismod. 
+		</p>
+	</div>
+) 
