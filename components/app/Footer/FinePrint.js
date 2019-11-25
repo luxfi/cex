85d23d1c76729bfd01e9f92adf35640e4bcd27b2
Zach Kelling <z@zeekay.io>
2019-11-25 15:51:11 -0800

    Update footer disclosure with valid links

--- components/app/Footer/FinePrint.js
+++ components/app/Footer/FinePrint.js
@@ -9,10 +9,10 @@ export default (props) => (
 		</p>
 		<p>
             All broker - dealer related securities activity is conducted by
-            Proxima Capital Management LLC, a registered broker - dealer, and member of
-            FINRA | SiPC, located at 2121 Avenue of the Stars, 23rd Floor, Los Angeles,
-            CA, 90067, please check our background on
-            <a href="https://www.adviserinfo.sec.gov/IAPD/IAPDFirmSummary.aspx?ORG_PK=133043">FINRA's BrokerCheck</a>.
+            Proxima Capital Management LLC, a registered broker - dealer, and member
+            of <a href="https://www.finra.org">FINRA</a> | <a href="https://www.sipc.org/">SiPC</a>,
+            located at 2121 Avenue of the Stars, 23rd Floor, Los Angeles,
+            CA, 90067, please check our background on <a href="https://www.adviserinfo.sec.gov/IAPD/IAPDFirmSummary.aspx?ORG_PK=133043">FINRA's BrokerCheck</a>.
             Proxima Capital does not make investment recommendations and no
             communication, through this Site or in any other medium should be construed
             as a recommendation for any security offered on or off this investment
@@ -27,36 +27,29 @@ export default (props) => (
             ESX LLC nor any of their officers, directors, agents and employees
             makes any warranty, express or implied, of any kind whatsoever related to the
             adequacy, accuracy or completeness of any information on this Site or the use
-            of information on this site.Offers to sell securities can only be made
+            of information on this site. Offers to sell securities can only be made
             through official offering documents that contain important information about
-            the investment and the issuers, including risks.Investors should carefully
-            read the offering documents.Investors should conduct their own due diligence
+            the investment and the issuers, including risks. Investors should carefully
+            read the offering documents. Investors should conduct their own due diligence
             and are encouraged to consult with their tax, legal and financial advisors.
 		</p>
 		<p>
             By accessing the Site and any pages thereof, you agree to be bound
-            by the Terms of Use and Privacy Policy. Please also see the Additional Risk
-            Disclosures.
+            by the <a href="/terms">Terms of Use</a> and <a href="/privacy">Privacy Policy</a>.
+            Please also see the <a href="/risks">Additional Risk Disclosures</a>.
 		</p>
 		<p>
-            Nothing on this site constitutes advice to buy or sell securities.
-            SEC registration does not constitute an endorsement by the SEC or FINRA, and
-            does not imply a level of skill or training. All activities on this site are
-            governed by our Terms of Service. Arbitration is the sole dispute resolution
-            method for any legal matter involving ESX or its users.
-		</p>
-		<p>
-            Investments in entertainment projects are particularly risky and
-            may result in total loss of invested capital. Past performance of a security
-            or IP does not guarantee future results or returns. Only investors who
-            understand the risks of early stage investment and who meet the ESX's
-            investment criteria may invest.
+            Investment in entertainment is particularly risky and may result in
+            total loss of invested capital. Past performance of a security or IP does not
+            guarantee future results or returns. Only investors who understand the risks
+            of early stage investment and who meet the ESX's investment criteria may
+            invest.
 		</p>
 		<p>
             ESX does not verify information provided by companies on this
             Portal and makes no assurance as to the completeness or accuracy of any such
             information. Additional information about projects fundraising on the Portal
-            can be found by searching the EDGAR database.
+            can be found by searching the <a href="https://www.sec.gov/edgar/searchedgar/companysearch.html">EDGAR database</a>.
 		</p>
 	</div>
 )
