be18cd5883a4eee622f4894902a8e632d7743ce5
David Tai <david@verus.io>
2019-11-29 10:57:37 -0600

    update eslint stuff

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: ['airbnb', 'prettier/react'],
+  plugins: ['autofix', 'import-order-alphabetical'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  rules: {
+    'jsx-a11y/href-no-hash': ['off'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-quotes': ['warn', 'prefer-single'],
+    'import-order-alphabetical/order': 'warn',
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": true,
+      "optionalDependencies": false,
+      "peerDependencies": false
+    }],
+    "sort-imports": ["warn", {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": true,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+    }],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
+      },
+    ],
+    'semi': ['error', 'never'],
+    'autofix/no-debugger': 'error',
+  }
+};
--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-const withCSS = require("@zeit/next-css")
-const withSass = require("@zeit/next-sass")
-const withStylus = require("@zeit/next-stylus")
-const rupture = require("rupture")
-const postcss = require("poststylus")
-const autoprefixer = require("autoprefixer")
-const comments = require("postcss-discard-comments")
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+const withStylus = require('@zeit/next-stylus')
+const autoprefixer = require('autoprefixer')
+const comments = require('postcss-discard-comments')
+const postcss = require('poststylus')
+const rupture = require('rupture')
+const articlesFromJson = require('./assets/tempData/articles')
+const moviesFromJson = require('./assets/tempData/movies')
 
 module.exports = withBundleAnalyzer(
   withCSS(
@@ -18,81 +20,85 @@ module.exports = withBundleAnalyzer(
             rupture(),
             postcss([
               autoprefixer(),
-              "rucksack-css",
-              comments({ removeAll: true })
-            ])
-          ]
+              'rucksack-css',
+              comments({ removeAll: true }),
+            ]),
+          ],
         },
-        webpack: (config, { defaultLoaders, isServer }) => {
+        webpack: (config) => {
           config.module.rules.push({
             test: /\.mjs$/,
-            type: "javascript/auto"
+            type: 'javascript/auto',
           })
 
           config.module.rules.push({
             test: /\.(eot|woff|woff2|ttf|webp|txt|jpg|png|jpeg|svg|gif)$/,
             use: [
               {
-                loader: "file-loader",
+                loader: 'file-loader',
                 options: {
-                  context: "",
-                  outputPath: "static",
-                  publicPath: "_next/static",
-                  name: "[path][name].[hash].[ext]"
-                }
-              }
-            ]
+                  context: '',
+                  outputPath: 'static',
+                  publicPath: '_next/static',
+                  name: '[path][name].[hash].[ext]',
+                },
+              },
+            ],
           })
 
           return config
         },
-        async exportPathMap () {
-          const articlesFromJson = require('./assets/tempData/articles')
-          const moviesFromJson = require('./assets/tempData/movies')
-
+        async exportPathMap() {
           const articles = articlesFromJson.reduce(
-            (articles, article) => {
-              return Object.assign({}, articles, {
-                [`/article/${article.articleSlug}`]: {
-                  page: '/article',
-                  query: { slug: article.articleSlug }
-                }
-              })
-            },
-            {}
+            (as, a) => ({
+              ...as,
+              [`/article/${a.articleSlug}`]: {
+                page: '/article',
+                query: { slug: a.articleSlug },
+              },
+            }),
+            {},
           )
 
           const movies = moviesFromJson.reduce(
-            (movies, movie) =>
-              Object.assign({}, movies, {
-                [`/film/${movie.movieSlug}`]: {
-                  page: '/film',
-                  query: { slug: movie.movieSlug },
-                },
-                [`/trade/${movie.movieSlug}`]: {
-                  page: '/trade',
-                  query: { slug: movie.movieSlug },
-                },
-              }),
-            {}
+            (ms, m) => ({
+              ...ms,
+              [`/film/${m.movieSlug}`]: {
+                page: '/film',
+                query: { slug: m.movieSlug },
+              },
+              [`/trade/${m.movieSlug}`]: {
+                page: '/trade',
+                query: { slug: m.movieSlug },
+              },
+              [`/pro/${m.movieSlug}`]: {
+                page: '/pro',
+                query: { slug: m.movieSlug },
+              },
+            }),
+            {},
           )
 
           // combine the map of post pages with the home
-          return Object.assign({}, articles, movies, {
-            "/": { page: "/" },
-            "/login": { page: "/login" },
-            "/signup": { page: "/signup" },
-            "/portfolio": { page: "/portfolio" },
-            "/account": { page: "/account" },
-            "/account/kyc": { page: "/account/kyc" },
-            "/investorFaq": { page: "/investorFaq" },
-            "/projectFaq": { page: "/projectFaq" },
-            "/risks": { page: "/risks" },
-            "/contact": { page: "/contact" },
-            "/about": { page: "/about" }
-          });
-        }
-      })
-    )
-  )
+          return {
+            ...articles,
+            ...movies,
+            '/': { page: '/' },
+            '/login': { page: '/login' },
+            '/signup': { page: '/signup' },
+            '/portfolio': { page: '/portfolio' },
+            '/portfolio/rewards': { page: '/portfolio/rewards' },
+            '/account': { page: '/account' },
+            '/account/kyc': { page: '/account/kyc' },
+            '/investorFaq': { page: '/investorFaq' },
+            '/projectFaq': { page: '/projectFaq' },
+            '/risks': { page: '/risks' },
+            '/contact': { page: '/contact' },
+            '/about': { page: '/about' },
+            '/placeholder': { page: '/placeholder' },
+          }
+        },
+      }),
+    ),
+  ),
 )
