d86a6283d36ef6ee00b84cad34966540fab04045
Tyler Newman <tylerjnewman18@gmail.com>
2019-12-05 00:13:44 -0800

    Make expansion panels transparent

--- components/offering/DealTermsSection/DealTermsSection.js
+++ components/offering/DealTermsSection/DealTermsSection.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Box, Typography, Divider } from '@material-ui/core'
+import {
+  Box,
+  Typography,
+  Divider,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-const useStyles = makeStyles(theme => ({}))
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}))
+
+const useExpansionPanelStyles = makeStyles({
+  root: {
+    backgroundColor: 'transparent',
+  },
+})
 
 const DealTermsSection = () => {
   const classes = useStyles()
-  const bull = <span className={classes.bullet}>•</span>
-
+  const expansionClasses = useExpansionPanelStyles()
   return (
     <Box mb={4}>
       <Typography variant="h5">
@@ -14,12 +35,44 @@ const DealTermsSection = () => {
           Summary
         </Box>
       </Typography>
-      <Typography variant="h5">
-        <Box mb={2} mt={2} fontWeight="fontWeightBold">
-          A good logo works in the simplest form. It sis a memeorable
-          represention of your brand and inspires confidence in your customers.{' '}
-        </Box>
-      </Typography>
+      <div className={classes.root}>
+        <ExpansionPanel classes={{ root: expansionClasses.root }}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>
+              Expansion Panel 1
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel classes={{ root: expansionClasses.root }}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>
+              Expansion Panel 2
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
     </Box>
   )
 }
