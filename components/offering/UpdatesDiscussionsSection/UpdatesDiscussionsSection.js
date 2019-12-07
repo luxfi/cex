51c1b76ae3e07cb31e7e39e479a185df8cac7f7c
Tyler Newman <tylerjnewman18@gmail.com>
2019-12-06 22:09:33 -0800

    Create UpdatesContainer and DiscussionsContainer

--- components/offering/UpdatesDiscussionsSection/UpdatesDiscussionsSection.js
+++ components/offering/UpdatesDiscussionsSection/UpdatesDiscussionsSection.js
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import classNames from 'classnames'
-import {
-  Box,
-  Typography,
-  Divider,
-  Fab,
-  ButtonGroup,
-  Button,
-} from '@material-ui/core'
+import { Box, Typography, Divider, Fab } from '@material-ui/core'
 import { fade } from '@material-ui/core/styles'
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 import { makeStyles } from '@material-ui/core/styles'
-import EventNoteIcon from '@material-ui/icons/EventNote'
+import { UpdatesContainer, DiscussionsContainer } from '../'
+import Collapse from '@material-ui/core/Collapse'
 
 const useStyles = makeStyles(theme => ({
   hover: {
@@ -20,7 +12,7 @@ const useStyles = makeStyles(theme => ({
   pointer: {
     cursor: 'pointer',
   },
-  toggleButtonGroup: {
+  viewButtonGroup: {
     backgroundColor: 'transparent',
   },
   root: {
@@ -53,7 +45,7 @@ const useStyles = makeStyles(theme => ({
   selected: {},
 }))
 
-const Updates = [
+const UPDATES = [
   {
     date: 'December 6th 2019',
     heading: 'Investors are bringing the rain!',
@@ -74,11 +66,30 @@ const Updates = [
   },
 ]
 
+const DISCUSSIONS = [
+  {
+    date: 'December 6th 2019',
+    userName: 'Lawrence Doyle',
+    avatarSrc: 'https://i.pravatar.cc/300',
+    details:
+      'How many free autoresponders have you tried? Really how many? And how many emails did you get through using them? How do you know? How many people opened your followup message? My point here is that if you have no clue for the answers above you probably are not operating a followup campaign successfully.',
+    like: true,
+  },
+  {
+    date: 'December 6th 2019',
+    userName: 'Sir Arthur Conan',
+    avatarSrc: 'https://i.pravatar.cc/300',
+    details:
+      'How many free autoresponders have you tried? Really how many? And how many emails did you get through using them? How do you know? How many people opened your followup message? My point here is that if you have no clue for the answers above you probably are not operating a followup campaign successfully.',
+    like: true,
+  },
+]
+
 const UpdatesDiscussionsSection = () => {
-  const [toggle, setToggle] = React.useState('updates')
+  const [view, toggleView] = React.useState('updates')
 
   const handleChange = (event, newToggle) => {
-    if (newToggle) setToggle(newToggle)
+    if (newToggle) toggleView(newToggle)
   }
   const classes = useStyles()
   return (
@@ -91,10 +102,10 @@ const UpdatesDiscussionsSection = () => {
         </Box>
         <Box flexShrink={1}>
           <ToggleButtonGroup
-            value={toggle}
+            value={view}
             exclusive
             onChange={handleChange}
-            className={classes.toggleButtonGroup}
+            className={classes.viewButtonGroup}
           >
             <ToggleButton
               classes={{ root: classes.root, selected: classes.selected }}
@@ -113,57 +124,12 @@ const UpdatesDiscussionsSection = () => {
           </ToggleButtonGroup>
         </Box>
       </Box>
-      {Updates.map(({ title, date, heading, details }, i) => {
-        const [hover, setHover] = useState(false)
-        const handleMouseOver = () => {
-          setHover(true)
-        }
-        const handleMouseOut = () => {
-          setHover(false)
-        }
-        return (
-          <div key={i}>
-            <Box display="flex" mb={1} mt={5}>
-              <Box flexShrink={1} mr={2.5}>
-                <Fab size="medium" color="secondary" aria-label="add">
-                  <EventNoteIcon />
-                </Fab>
-              </Box>
-              <Box width="100%">
-                <Typography
-                  variant="body1"
-                  component="span"
-                  color="textSecondary"
-                >
-                  {date}
-                </Typography>
-
-                <Typography
-                  variant="h5"
-                  onMouseOver={() => handleMouseOver()}
-                  onMouseOut={() => handleMouseOut()}
-                  className={classNames(classes.pointer, {
-                    [classes.hover]: hover,
-                  })}
-                >
-                  <Box mb={1} fontWeight="fontWeightBold">
-                    {heading}
-                  </Box>
-                </Typography>
-                <Typography
-                  variant="body1"
-                  color="textSecondary"
-                  component="div"
-                >
-                  <Box mb={5}>{details}</Box>
-                </Typography>
-              </Box>
-            </Box>
-
-            <Divider />
-          </div>
-        )
-      })}
+      <Collapse in={view === 'updates'}>
+        <UpdatesContainer updates={UPDATES} />
+      </Collapse>
+      <Collapse in={view === 'discussions'}>
+        <DiscussionsContainer discussions={DISCUSSIONS} />
+      </Collapse>
     </Box>
   )
 }
