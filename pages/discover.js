ae2f9965615fec50d30eacb256e6f3fd952d335b
artemis-prime <artemisprimedev@gmail.com>
2019-12-18 08:13:36 -0800

    new hero start

--- pages/discover.js
+++ pages/discover.js
@@ -1,27 +1,27 @@
-import { makeStyles } from '@material-ui/core/styles'
-import { inject, observer } from 'mobx-react'
-
 import React, { useEffect } from 'react'
+import { inject, observer } from 'mobx-react'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { googlePageView } from '../util/generic'
 
 import {
   CategorySlider,
   ForYouSlider,
-  Hero,
   StudioSlider,
   TrailerSlider,
 } from '../components/landing'
-
-import { googlePageView } from '../util/generic'
 
 const heroMaxHeight = 800
 const heroMinHeight = 600
 
 const useStyles = makeStyles(() => ({
+  /*
   hero: {
     height: '90vh',
     maxHeight: heroMaxHeight,
     minHeight: heroMinHeight,
   },
+  */
 }))
 
 export default inject('store')(observer((props) => {
@@ -34,12 +34,7 @@ export default inject('store')(observer((props) => {
 
   return (
     <>
-      <Hero
-        className={classes.hero}
-        maxHeight={heroMaxHeight}
-        minHeight={heroMinHeight}
-      />
-      <div style={{ marginTop: -120, position: 'relative' }}>
+      <div style={{ /*marginTop: -120,*/ position: 'relative' }}>
         <TrailerSlider/>
         <ForYouSlider />
         <StudioSlider />
