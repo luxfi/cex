f987c2e8980b53a290b4354abec9fb61d0e092ad
David Tai <david@verus.io>
2019-12-18 11:11:54 -0600

    add new movies, random lint fixes

--- components/account/BalanceHistoryItem/BalanceHistoryItem.js
+++ components/account/BalanceHistoryItem/BalanceHistoryItem.js
@@ -1,45 +1,44 @@
-import React from 'react'
 import {
   Divider,
   Grid,
-  Typography
- } from '@material-ui/core'
+  Typography,
+} from '@material-ui/core'
 
-export default props => {
-  const { 
+import React from 'react'
+
+export default (props) => {
+  const {
     name,
     amount,
     date,
-    deposit
+    deposit,
   } = props
 
   return (
     <Grid item xs={12}>
-      <Grid container direction="column" spacing={0}>
+      <Grid container direction='column' spacing={0}>
         <Divider style={{ marginBottom: '15px' }} />
         <Grid container>
           <Grid item xs={8}>
-            <Typography variant="h5">
+            <Typography variant='h5'>
               {deposit ? 'Deposit from' : 'Withdrawal to'} {name}
             </Typography>
-            <Typography variant="h6">
+            <Typography variant='h6'>
               {date}
             </Typography>
           </Grid>
           <Grid item xs={4}>
-            {
-              deposit ?
-                <Typography variant={'h6'} style={{ color: 'green' }}>
-                  + ${amount}
-                </Typography>
-                :
-                <Typography variant={'h6'} style={{ color: 'red' }}>
-                  - ${amount}
-                </Typography>
+            { deposit
+              ? <Typography variant='h6' style={{ color: 'green' }}>
+                + ${amount}
+              </Typography>
+              : <Typography variant='h6' style={{ color: 'red' }}>
+                - ${amount}
+              </Typography>
             }
           </Grid>
         </Grid>
       </Grid>
     </Grid>
-  );
+  )
 }
