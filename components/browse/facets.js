ab2293dca9680f3220717ca4ccf7ee3b6f1199d7
artemis-prime <artemisprimedev@gmail.com>
2019-12-24 16:22:02 -0800

    facet menu better UI

--- components/browse/facets.js
+++ components/browse/facets.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import classNames from 'classnames'
 
 import {
   Button,
   Icon,
+  IconButton,
   MenuItem,
   MenuList,
   Paper,
@@ -10,6 +12,12 @@ import {
   Typography,
   makeStyles,
 } from '@material-ui/core'
+
+import {
+  Close,
+  Cancel,
+  Check,
+} from '@material-ui/icons'
 
   // This module is recommended in the MUI docs themselves :)
 import PopupState, { bindTrigger, bindPopper } from 'material-ui-popup-state'
@@ -20,22 +28,51 @@ const useStyles = makeStyles(styles)
 const genreFacet = {
   name: 'genre',
   titleAll: 'All Genres',
-  titleSome: 'Genres',
-  values: 
-    [
-      {
-        title: 'Action',
-        active: false,
-      },
-      {
-        title: 'Adventure',
-        active: true,
-      },
-      {
-        title: 'Sci-Fi',
-        active: false,
-      },
-    ]
+  titleSome: 'Genre',
+  values: {
+    action: {
+      title: 'Action',
+      color: '#2676ee',
+      gradient: 'linear-gradient(90deg, #015ce3 0%, #4a90f9 100%)',
+      active: false,
+    },
+    comedy: {
+      title: 'Comedy',
+      color: '#24c6ea',
+      gradient: 'linear-gradient(90deg, #02b0d7 0%, #46dbfc 100%)',
+      active: true,
+    },
+    documentary: {
+      title: 'Documentary',
+      color: '#47d4ba',
+      gradient: 'linear-gradient(90deg, #26c3ac 0%, #69e6c8 100%)',
+      active: false,
+    },
+    drama: {
+      title: 'Drama',
+      color: '#76dd7b',
+      gradient: 'linear-gradient(90deg, #6bc959 0%, #80f09b 100%)',
+      active: false,
+    },
+    romance: {
+      title: 'Romance',
+      color: '#f3913d',
+      gradient: 'linear-gradient(90deg, #e77718 0%, #ffaa61 100%)',
+      active: true,
+    },
+    scifi: {
+      title: 'Sci-Fi',
+      color: '#ef4343',
+      gradient: 'linear-gradient(90deg, #e01717 0%, #fe7070 100%)',
+      active: false,
+    },
+    thriller: {
+      title: 'Thriller',
+      color: '#ad4bc3',
+      gradient: 'linear-gradient(90deg,  #8c3b9e 0%, #cf5bea 100%)',
+      active: false,
+    },
+  }
 } 
 
 const Facets = (props) => {
@@ -48,54 +85,75 @@ const Facets = (props) => {
   )
 } 
 
-const Facet = ({facet, classes }) => {
+const Facet = ({facet, toggleFacetFn, classes }) => {
   
-  const activeValues = facet.values.filter(f => f.active)
+  let activeValues = []
+  Object.keys(facet.values).forEach( (f, i) => {
+    if (facet.values[f].active) {
+      activeValues.push(facet.values[f])
+    } 
+  })
+
   const title = activeValues.length ? facet.titleSome + ':' : facet.titleAll
 
   return (
-    <div className={classes.facetOuter}>
+    <Paper className={classes.facetOuter}>
       <PopupState variant='popper' popupId='menu-popover'>
-        {(popupState) => (
-          <>
-          <Button
-            {...bindTrigger(popupState)}
-            className={classes.facetDropdownButton}
-            classes={{
-              label: classes.facetButtonText
-            }}
-          >
-            {title}
-          </Button >
-          <Popper
-            {...bindPopper(popupState)}
-            className={classes.facetValuesMenu}
-            transition
-            placement='bottom-end'
-          >
-            <Paper>
-              <MenuList>
-              {facet.values.map((v ) => (
-                <MenuItem onClick={popupState.close} className={classes.facetMenuValueOuter} key={v.title}>
-                  <Icon className={v.active ? classes.facetValueIconActive : classes.facetValueIconInactive}>check</Icon>
-                  <span className={classes.facetMenuValueTitle}>
-                    {v.title}
-                  </span>
-                </MenuItem>
-              ))}
-              </MenuList>
-            </Paper>
-          </Popper>
-          </>
-        )}
+        {(popupState) => {
+
+          const toggleFacet = () => {
+            //toggleFacetFn()
+            popupState.close()
+          }
+
+          const valueMarkup = []
+          Object.keys(facet.values).forEach((v, i) => {
+            const val = facet.values[v]
+            valueMarkup.push(
+              <MenuItem onClick={toggleFacet} className={classes.facetValueMenuItemOuter} key={val.title}>
+                <Check className={classNames(val.active ? classes.facetValueIconActive : classes.facetValueIconInactive, classes.facetValueIcon)} />
+                <span className={classes.facetValueTitle} style={{borderBottomColor: val.color}}>
+                  {val.title}
+                </span>
+              </MenuItem>
+            )
+          })
+          return (
+            <>
+            <Button
+              {...bindTrigger(popupState)}
+              className={classes.facetDropdownButton}
+              classes={{ label: classes.facetButtonText }}
+            >
+              {title}
+            </Button >
+            <Popper
+              {...bindPopper(popupState)}
+              className={classes.facetValuesMenu}
+              transition
+              placement='bottom-end'
+            >
+              <Paper>
+                <MenuList>
+                  {valueMarkup}
+                </MenuList>
+              </Paper>
+            </Popper>
+            </>
+          )
+        }}
       </PopupState>
       {activeValues.map(v => (
         <div className={classes.activeFacetPill} key={v.title}>
-          <span className={classes.activeFacetTitle}>{v.title}</span>
-          <Icon className={classes.activeFacetCloseIcon}>cancel</Icon>
+          <div className={classes.activeFacetPillInner} >
+            <span className={classes.activeFacetTitle} style={{borderBottomColor: v.color}}>{v.title}</span>
+            <IconButton className={classes.activeFacetCloseButton} onClick={toggleFacetFn}>
+              <Close className={classes.activeFacetCloseButtonIcon} />
+            </IconButton>
+          </div>
         </div>
       ))}
-    </div>
+    </Paper>
   )
 }
 
--- components/browse/facets.style.js
+++ components/browse/facets.style.js
@@ -4,23 +4,25 @@ export default theme => ({
     marginLeft: 'auto',
     display: 'flex',
     flexDirection: 'row',
-    //justifyContent: 'center',
-    //alignContent: 'center',
+    alignItems: 'center',
   },
   facetsLabel: {
     display: 'block',
-    marginRight: theme.spacing(3)
+    marginRight: theme.spacing(3),
   },
   
 
   facetOuter: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: theme.spacing(1),
+    borderRadius: '9px',
   },
 
   facetDropdownButton: {
     backgroundColor: '#222',
-    marginRight: theme.spacing(2),
+    borderRadius: '9px',
     '&:last-child': {
       marginRight: 0
     },
@@ -35,10 +37,64 @@ export default theme => ({
     zIndex: 20
   },
   
+  facetValueMenuItemOuter:{
+    paddingLeft: '10px'
+  },
+
+  facetValueIcon: {
+    fontSize: '0.85rem',
+    lineHeight: 'inherit',
+    marginRight: '6px',
+  },
   facetValueIconInactive: {
       // using visibility: hidden rather than display: none so layout is preserved
     visibility: 'hidden'
   },
+  facetValueTitle: {
+    fontSize: '0.9rem',
+    lineHeight: 'inherit',
+      // border color is set when rendered
+    borderBottomThickness: '4px',
+    borderBottomStyle: 'solid',
+    minWidth: theme.spacing(11)
+  },
+
+  activeFacetPill: {
+    backgroundColor: '#2f2f2f',
+    position: 'relative',
+    top: '1.5px',
+    paddingLeft: '8px',
+    paddingRight: '2px',
+    paddingBottom: '2px',
+    borderRadius: '9px',
+    marginRight: theme.spacing(0.6),
+    '&:last-child': {
+      marginRight: 0
+    }
+  },
+  activeFacetPillInner: {
+  },
+  activeFacetTitle: {
+    fontSize: '0.75rem',
+      // border color is set when rendered
+    borderBottomThickness: '3px',
+    borderBottomStyle: 'solid',
+    borderRadius: '2px',
+  },
+  activeFacetCloseButton: {
+    fontSize: '0.75rem',
+    padding: '2px',
+    marginLeft: '2px',
+    opacity: 0.3,
+    '&:hover': {
+      opacity: 1,
+      backgroundColor: '#3f3f3f',
+    }
+  },
+  activeFacetCloseButtonIcon: {
+    verticalAlign: 'middle',
+    fontSize: 'inherit',
+  }
 })
 
 
