f93112f38b42026765c8a02e4d96361ef4e8beba
artemis prime <artemisprimedev@gmail.com>
2020-04-04 12:52:28 -0700

    same

--- components/app/Header/HeaderLogo.js
+++ components/app/Header/HeaderLogo.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { makeStyles } from '@material-ui/core'
 
 const LOGO = '/static/images/esx/logo-esx-mixed-color-72x42.png'
@@ -21,16 +19,3 @@ export default (props) => {
     <div className={s.root} />
   )
 }
-
-/*
-
-        <img src='/static/images/esx/u1.png' alt='ESX' className={classes.logo} height='90px' />
-
-            <img
-              src='/static/images/esx/esx-white-logo.png'
-              alt='ESX'
-              className={classes.mobileLogo}
-              height='26px'
-            />
-
-*/
